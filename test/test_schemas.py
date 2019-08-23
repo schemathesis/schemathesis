e42b2cbf610422d626d536e043b8e17c89d6afd8
Dmitry Dygalo <dadygalo@gmail.com>
2019-08-23 21:37:51 +0200

    Add test for traverse_schema

--- test/test_schemas.py
+++ test/test_schemas.py
@@ -1,6 +1,7 @@
 import pytest
 
 from schemathesis import SwaggerV20
+from schemathesis.schemas import traverse_schema
 
 
 @pytest.fixture()
@@ -14,3 +15,19 @@ def test_base_path_suffix(swagger_20, base_path):
     swagger_20.raw_schema["basePath"] = base_path
     # Then base path ends with "/" anyway in the swagger instance
     assert swagger_20.base_path == "/v1/"
+
+
+def test_traverse_schema(simple_schema):
+    assert list(traverse_schema(simple_schema)) == [
+        (["swagger"], "2.0"),
+        (["info", "title"], "Sample API"),
+        (["info", "description"], "API description in Markdown."),
+        (["info", "version"], "1.0.0"),
+        (["host"], "api.example.com"),
+        (["basePath"], "/v1"),
+        (["schemes"], ["https"]),
+        (["paths", "/users", "get", "summary"], "Returns a list of users."),
+        (["paths", "/users", "get", "description"], "Optional extended description in Markdown."),
+        (["paths", "/users", "get", "produces"], ["application/json"]),
+        (["paths", "/users", "get", "responses", 200, "description"], "OK"),
+    ]
