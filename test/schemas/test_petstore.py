7c33a2cdb848c17788180c5c8cd8bcf26ac7cb67
Dmitry Dygalo <dadygalo@gmail.com>
2019-08-23 22:32:36 +0200

    Implement a smoke test for petstore schema

--- test/schemas/test_petstore.py
+++ test/schemas/test_petstore.py
@@ -1,8 +1,16 @@
-# from .utils import get_schema
-#
-# petstore = get_schema("petstore.yaml")
-#
-#
-# @petstore.parametrize()
-# def test_petstore(case):
-#     print(case.method, case.formatted_path, case.query, case.body)
+def test_petstore(testdir):
+    # Smoke test for petstore.yaml
+    # TODO. Should be extended to verify that all data is generated correctly
+    testdir.make_test(
+        """
+@schema.parametrize(max_examples=5)
+def test_(request, case):
+    request.config.HYPOTHESIS_CASES += 1
+""",
+        schema_name="petstore.yaml",
+    )
+    result = testdir.runpytest("-v")
+    result.assert_outcomes(passed=20)
+    # GET:/v2/store/inventory and GET:/v2/users/logout have only 1 case each,
+    # then 18 * 5 + 2 = 92
+    result.stdout.re_match_lines([r"Hypothesis calls: 92"])
