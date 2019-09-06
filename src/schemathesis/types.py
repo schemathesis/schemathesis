0dd31165b79c7596ed040d452f10a4a2bb159b04
Dmitry Dygalo <dadygalo@gmail.com>
2019-09-06 15:24:07 +0200

    Simplify & fix types

--- src/schemathesis/types.py
+++ src/schemathesis/types.py
@@ -4,10 +4,10 @@ from typing import Any, Dict, List, NewType, Set, Tuple, Union
 Schema = NewType("Schema", Dict[str, Any])
 PathLike = Union[Path, str]
 
-Query = NewType("Query", List[Tuple[str, str]])  # To use in HTTP client
-Body = NewType("Body", Dict[str, Any])
-PathParameters = NewType("PathParameters", Dict[str, Any])
-Headers = NewType("Headers", Dict[str, Any])
+Query = Dict[str, Any]
+Body = Dict[str, Any]
+PathParameters = Dict[str, Any]
+Headers = Dict[str, Any]
 
 # A filter for endpoint / method
 Filter = Union[str, List[str], Tuple[str], Set[str]]
