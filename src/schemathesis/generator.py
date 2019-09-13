ceb121a02996ef0a3e12a352741e4af3724fee99
Dmitry Dygalo <dmitry.dygalo@kiwi.com>
2019-09-13 17:28:39 +0200

    Generating explicit examples from schema. Closes #17

--- src/schemathesis/generator.py
+++ src/schemathesis/generator.py
@@ -1,13 +1,16 @@
 """Provide strategies for given endpoint(s) definition."""
-from typing import Callable
+from typing import Callable, Generator
 
 import attr
 import hypothesis.strategies as st
-from hypothesis import given
+from hypothesis import example, given
+from hypothesis._strategies import just
 from hypothesis_jsonschema import from_schema
 
 from .schemas import Endpoint
 from .types import Body, Headers, PathParameters, Query
+
+PARAMETERS = {"path_parameters", "headers", "query", "body"}
 
 
 @attr.s(slots=True)
@@ -30,10 +33,36 @@ class Case:
 def create_hypothesis_test(endpoint: Endpoint, test: Callable) -> Callable:
     """Create a Hypothesis test."""
     strategy = get_case_strategy(endpoint)
-    return given(case=strategy)(test)
+    wrapped_test = given(case=strategy)(test)
+    return add_examples(wrapped_test, endpoint)
+
+
+def get_examples(endpoint: Endpoint) -> Generator[Case, None, None]:
+    for name in PARAMETERS:
+        parameter = getattr(endpoint, name)
+        if "example" in parameter:
+            other_parameters = {other: from_schema(getattr(endpoint, other)) for other in PARAMETERS - {name}}
+            yield st.builds(
+                Case,
+                path=st.just(endpoint.path),
+                method=st.just(endpoint.method),
+                **{name: just(parameter["example"])},
+                **other_parameters
+            ).example()
+
+
+def add_examples(test: Callable, endpoint: Endpoint) -> Callable:
+    """Add examples to the Hypothesis test, if they are specified in the schema."""
+    for case in get_examples(endpoint):
+        test = example(case)(test)
+    return test
 
 
 def get_case_strategy(endpoint: Endpoint) -> st.SearchStrategy:
+    """Create a strategy for a complete test case.
+
+    Path & endpoint are static, the others are JSON schemas.
+    """
     return st.builds(
         Case,
         path=st.just(endpoint.path),
--- test/test_generator.py
+++ test/test_generator.py
@@ -1,3 +1,42 @@
+from unittest.mock import ANY
+
+import pytest
+
+from schemathesis.generator import PARAMETERS, Case, get_examples
+from schemathesis.schemas import Endpoint
+
+
 def test_path(case_factory):
     case = case_factory(path="/users/{name}", path_parameters={"name": "test"})
     assert case.formatted_path == "/users/test"
+
+
+def _make(cls, default, **kwargs):
+    for parameter in PARAMETERS:
+        kwargs.setdefault(parameter, default)
+    return cls("/users", "GET", **kwargs)
+
+
+def make_endpoint(**kwargs):
+    return _make(Endpoint, {}, **kwargs)
+
+
+def make_case(**kwargs):
+    return _make(Case, ANY, **kwargs)
+
+
+@pytest.mark.parametrize("name", PARAMETERS)
+def test_get_examples(name):
+    example = {"name": "John"}
+    endpoint = make_endpoint(
+        **{
+            name: {
+                "required": ["name"],
+                "type": "object",
+                "additionalProperties": False,
+                "properties": {"name": {"type": "string"}},
+                "example": example,
+            }
+        }
+    )
+    assert list(get_examples(endpoint)) == [make_case(**{name: example})]
