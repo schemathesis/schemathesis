c69f8452325739b745e8db3f47883310b7591503
dmitry.dygalo <dmitry.dygalo@kiwi.com>
2019-09-24 13:48:31 +0200

    Remove substring match for method / endpoint filters.

--- src/schemathesis/filters.py
+++ src/schemathesis/filters.py
@@ -16,8 +16,4 @@ def should_skip_endpoint(endpoint: str, pattern: Optional[Filter]) -> bool:
     if pattern is None:
         return False
     patterns = force_tuple(pattern)
-    return not any(is_match(endpoint, item) for item in patterns)
-
-
-def is_match(endpoint: str, pattern: str) -> bool:
-    return pattern in endpoint or bool(re.search(pattern, endpoint))
+    return not any(re.search(item, endpoint) for item in patterns)
