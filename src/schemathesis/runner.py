9212d74ebb5a36750afcf3d990f12a0341c8552f
Dmitry Dygalo <dmitry.dygalo@kiwi.com>
2019-09-25 15:43:55 +0200

    Refactor runner, add aiohttp dependency

--- src/schemathesis/runner.py
+++ src/schemathesis/runner.py
@@ -1,21 +1,35 @@
-from typing import Callable
+from typing import Callable, NoReturn
+from urllib.parse import urlsplit, urlunsplit
 
 import requests
 
+from .loaders import from_uri
 from .models import Case
-from .schemas import BaseSchema
 
 
-def execute(url: str, schema: BaseSchema) -> None:
-    def single_test(case: Case, check: Callable) -> None:
-        response = requests.request(
-            case.method, f"{url}{case.formatted_path}", headers=case.headers, params=case.query, json=case.body
-        )
-        check(response)
-
-    for _, test in schema.get_all_tests(single_test):
-        test(check_1)
+def execute(schema_uri: str) -> NoReturn:
+    with requests.Session() as session:
+        schema = from_uri(schema_uri)
+        base_url = get_base_url(schema_uri)
+        for endpoint, test in schema.get_all_tests(single_test):
+            test(session, base_url, not_a_server_error)
 
 
-def check_1(response: requests.Response) -> None:
+def get_base_url(uri: str) -> str:
+    parts = urlsplit(uri)[:2] + ("", "", "")
+    return urlunsplit(parts)
+
+
+def single_test(case: Case, session: requests.Session, url: str, check: Callable) -> NoReturn:
+    response = get_response(session, url, case)
+    check(response)
+
+
+def get_response(session: requests.Session, url: str, case: Case) -> requests.Response:
+    return session.request(
+        case.method, f"{url}{case.formatted_path}", headers=case.headers, params=case.query, json=case.body
+    )
+
+
+def not_a_server_error(response: requests.Response) -> NoReturn:
     assert response.status_code < 500
