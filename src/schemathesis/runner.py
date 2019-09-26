fe6d500c00b43f5037e74fccf1c81e5dfd028ee8
dmitry.dygalo <dmitry.dygalo@kiwi.com>
2019-09-26 18:57:58 +0200

    Support custom checks for `runner`

--- src/schemathesis/runner.py
+++ src/schemathesis/runner.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Iterator
 from urllib.parse import urlsplit, urlunsplit
 
 import requests
@@ -7,13 +7,18 @@ from .loaders import from_uri
 from .models import Case
 
 
-def execute(schema_uri: str) -> None:
+def not_a_server_error(response: requests.Response) -> None:
+    """A check to verify that the response is not a server-side error."""
+    assert response.status_code < 500
+
+
+def execute(schema_uri: str, checks=(not_a_server_error,)) -> None:
     """Generate and run test cases against the given API definition."""
     with requests.Session() as session:
         schema = from_uri(schema_uri)
         base_url = get_base_url(schema_uri)
         for _, test in schema.get_all_tests(single_test):
-            test(session, base_url, not_a_server_error)
+            test(session, base_url, checks)
 
 
 def get_base_url(uri: str) -> str:
@@ -22,10 +27,11 @@ def get_base_url(uri: str) -> str:
     return urlunsplit(parts)
 
 
-def single_test(case: Case, session: requests.Session, url: str, check: Callable) -> None:
+def single_test(case: Case, session: requests.Session, url: str, checks: Iterator[Callable]) -> None:
     """A single test body that will be executed against the target."""
     response = get_response(session, url, case)
-    check(response)
+    for check in checks:
+        check(response)
 
 
 def get_response(session: requests.Session, url: str, case: Case) -> requests.Response:
@@ -33,8 +39,3 @@ def get_response(session: requests.Session, url: str, case: Case) -> requests.Re
     return session.request(
         case.method, f"{url}{case.formatted_path}", headers=case.headers, params=case.query, json=case.body
     )
-
-
-def not_a_server_error(response: requests.Response) -> None:
-    """A check to verify that the response is not a server-side error."""
-    assert response.status_code < 500
