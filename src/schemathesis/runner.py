4f8ae20a922a986d335ef6c8bd935cd8eac8534e
dmitry.dygalo <dmitry.dygalo@kiwi.com>
2019-09-26 18:36:50 +0200

    Add docstrings, comments & small refactorings.

--- src/schemathesis/runner.py
+++ src/schemathesis/runner.py
@@ -8,6 +8,7 @@ from .models import Case
 
 
 def execute(schema_uri: str) -> None:
+    """Generate and run test cases against the given API definition."""
     with requests.Session() as session:
         schema = from_uri(schema_uri)
         base_url = get_base_url(schema_uri)
@@ -16,20 +17,24 @@ def execute(schema_uri: str) -> None:
 
 
 def get_base_url(uri: str) -> str:
+    """Remove the path part off the given uri."""
     parts = urlsplit(uri)[:2] + ("", "", "")
     return urlunsplit(parts)
 
 
 def single_test(case: Case, session: requests.Session, url: str, check: Callable) -> None:
+    """A single test body that will be executed against the target."""
     response = get_response(session, url, case)
     check(response)
 
 
 def get_response(session: requests.Session, url: str, case: Case) -> requests.Response:
+    """Send an appropriate request to the target."""
     return session.request(
         case.method, f"{url}{case.formatted_path}", headers=case.headers, params=case.query, json=case.body
     )
 
 
 def not_a_server_error(response: requests.Response) -> None:
+    """A check to verify that the response is not a server-side error."""
     assert response.status_code < 500
