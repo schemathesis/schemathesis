2c93ab28121996308968753b14ec3213101b67c1
Dmitry Dygalo <dmitry.dygalo@kiwi.com>
2019-09-27 12:47:41 +0200

    Add tests for cases discovered by mutmut

--- test/test_lazy.py
+++ test/test_lazy.py
@@ -73,6 +73,39 @@ def test_(request, case, another):
     result.stdout.re_match_lines([r"Hypothesis calls: 1"])
 
 
+def test_with_filters(testdir):
+    # When the test uses method / endpoint filter
+    testdir.make_test(
+        """
+@pytest.fixture
+def simple_schema():
+    return schema
+
+lazy_schema = schemathesis.from_pytest_fixture("simple_schema")
+
+@lazy_schema.parametrize(endpoint="/pets")
+def test_a(request, case):
+    request.config.HYPOTHESIS_CASES += 1
+    assert case.path == "/v1/pets"
+    assert case.method == "POST"
+
+@lazy_schema.parametrize(method="POST")
+def test_b(request, case):
+    request.config.HYPOTHESIS_CASES += 1
+    assert case.path == "/v1/pets"
+    assert case.method == "POST"
+""",
+        paths={"/pets": {"post": {}}},
+    )
+    result = testdir.runpytest("-v")
+    # Then the filters should be applied to the generated tests
+    result.assert_outcomes(passed=2)
+    result.stdout.re_match_lines(
+        [r"test_with_filters.py::test_a PASSED", r"test_with_filters.py::test_b PASSED", r".*2 passed"]
+    )
+    result.stdout.re_match_lines([r"Hypothesis calls: 2"])
+
+
 def test_invalid_fixture(testdir):
     # When the test uses a schema fixture that doesn't return a BaseSchema subtype
     testdir.make_test(
