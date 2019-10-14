14afb933c2f072b7d78f7edcb8fb7eb7a411b100
Dmitry Dygalo <dmitry.dygalo@kiwi.com>
2019-10-14 16:22:53 +0200

    Use "dev" package version if Schemathesis is run without installation

--- src/schemathesis/__init__.py
+++ src/schemathesis/__init__.py
@@ -1,6 +1,10 @@
-from importlib_metadata import version
+from importlib_metadata import PackageNotFoundError, version
 
 from .loaders import Parametrizer, from_dict, from_file, from_path, from_pytest_fixture, from_uri
 from .models import Case
 
-__version__ = version(__package__)
+try:
+    __version__ = version(__package__)
+except PackageNotFoundError:
+    # Local run without installation
+    __version__ = "dev"
