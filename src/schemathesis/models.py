08f0971db49557ac2e1f46a36725ed03021aff9f
Dmitry Dygalo <dmitry.dygalo@kiwi.com>
2019-10-14 17:56:57 +0200

    Add `Case.call` and `Case.as_requests_kwargs` convenience methods.

--- src/schemathesis/models.py
+++ src/schemathesis/models.py
@@ -1,10 +1,13 @@
 # pylint: disable=too-many-instance-attributes
-from typing import Any, Dict
+from typing import TYPE_CHECKING, Any, Dict
 
 import attr
 from hypothesis.searchstrategy import SearchStrategy
 
 from .types import Body, Cookies, FormData, Headers, PathParameters, Query
+
+if TYPE_CHECKING:
+    import requests  # Typechecking-only import to speedup import of schemathesis
 
 
 @attr.s(slots=True)
@@ -13,17 +16,35 @@ class Case:
 
     path: str = attr.ib()
     method: str = attr.ib()
-    path_parameters: PathParameters = attr.ib()
-    headers: Headers = attr.ib()
-    cookies: Cookies = attr.ib()
-    query: Query = attr.ib()
-    body: Body = attr.ib()
-    form_data: FormData = attr.ib()
+    path_parameters: PathParameters = attr.ib(factory=dict)
+    headers: Headers = attr.ib(factory=dict)
+    cookies: Cookies = attr.ib(factory=dict)
+    query: Query = attr.ib(factory=dict)
+    body: Body = attr.ib(factory=dict)
+    form_data: FormData = attr.ib(factory=dict)
 
     @property
     def formatted_path(self) -> str:
         # pylint: disable=not-a-mapping
         return self.path.format(**self.path_parameters)
+
+    def as_requests_kwargs(self, base_url: str) -> Dict[str, Any]:
+        """Convert the case into a dictionary acceptable by requests."""
+        return {
+            "method": self.method,
+            "url": f"{base_url}{self.formatted_path}",
+            "headers": self.headers,
+            "params": self.query,
+            "json": self.body,
+        }
+
+    def call(self, base_url: str, **kwargs: Any) -> "requests.Response":
+        """Convert the case into a dictionary acceptable by requests."""
+        # Local import to speedup import of schemathesis
+        import requests  # pylint: disable=import-outside-toplevel
+
+        data = self.as_requests_kwargs(base_url)
+        return requests.request(**data, **kwargs)
 
 
 def empty_object() -> Dict[str, Any]:
