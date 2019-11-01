b25c7f108d8b91b4cf3fd91ed8e88a706dd8f36c
Dmitry Dygalo <dadygalo@gmail.com>
2019-11-01 13:29:28 +0100

    Fix compatibility with Hypothesis 4.42.4+

--- test/test_compat.py
+++ test/test_compat.py
@@ -1,9 +1,11 @@
 import sys
 import warnings
 
+import hypothesis
 from hypothesis.errors import NonInteractiveExampleWarning
 
 from schemathesis._compat import handle_warnings
+from schemathesis._hypothesis import get_original_test
 
 
 def test_handle_warnings(recwarn):
@@ -18,3 +20,18 @@ def test_handle_warnings_old_hypothesis(monkeypatch, recwarn):
     with handle_warnings():
         warnings.warn("Message", NonInteractiveExampleWarning)
     assert recwarn
+
+
+def test_get_original_test_old_hypothesis(monkeypatch):
+    monkeypatch.setattr(hypothesis, "__version_info__", (4, 40, 0))
+
+    def original_test():
+        pass
+
+    def wrapped():
+        pass
+
+    wrapped._hypothesis_internal_settings_applied = True
+    wrapped._hypothesis_internal_test_function_without_warning = original_test
+
+    assert get_original_test(wrapped) is original_test
