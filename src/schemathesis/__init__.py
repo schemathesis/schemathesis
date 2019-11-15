c51cf258e72b8026ab5c1e8003591dbf6db6d5dc
Dmitry Dygalo <dadygalo@gmail.com>
2019-11-15 16:21:33 +0100

    Add a way to register custom checks for CLI

--- src/schemathesis/__init__.py
+++ src/schemathesis/__init__.py
@@ -1,4 +1,5 @@
 from ._hypothesis import init_default_strategies, register_string_format
+from .cli import register_check
 from .constants import __version__
 from .loaders import Parametrizer, from_dict, from_file, from_path, from_pytest_fixture, from_uri
 from .models import Case
