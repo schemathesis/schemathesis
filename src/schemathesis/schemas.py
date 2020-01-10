356cd39ebc1357bca0721d1d4c753196bf130e69
Stanislav Tkachenko <stanislav.tkachenko@kiwi.com>
2020-01-10 15:27:20 +0100

    chore: Improve error message when the schema is invalid

--- src/schemathesis/schemas.py
+++ src/schemathesis/schemas.py
@@ -145,22 +145,25 @@ class SwaggerV20(BaseSchema):
         return urljoin(self.base_path, path.lstrip("/"))  # pragma: no mutate
 
     def get_all_endpoints(self) -> Generator[Endpoint, None, None]:
-        paths = self.raw_schema["paths"]  # pylint: disable=unsubscriptable-object
-        for path, methods in paths.items():
-            full_path = self.get_full_path(path)
-            if should_skip_endpoint(full_path, self.endpoint):
-                continue
-            methods = self.resolve(methods)
-            common_parameters = get_common_parameters(methods)
-            for method, definition in methods.items():
-                if (
-                    method == "parameters"
-                    or should_skip_method(method, self.method)
-                    or should_skip_by_tag(definition.get("tags"), self.tag)
-                ):
+        try:
+            paths = self.raw_schema["paths"]  # pylint: disable=unsubscriptable-object
+            for path, methods in paths.items():
+                full_path = self.get_full_path(path)
+                if should_skip_endpoint(full_path, self.endpoint):
                     continue
-                parameters = itertools.chain(definition.get("parameters", ()), common_parameters)
-                yield self.make_endpoint(full_path, method, parameters, definition)
+                methods = self.resolve(methods)
+                common_parameters = get_common_parameters(methods)
+                for method, definition in methods.items():
+                    if (
+                        method == "parameters"
+                        or should_skip_method(method, self.method)
+                        or should_skip_by_tag(definition.get("tags"), self.tag)
+                    ):
+                        continue
+                    parameters = itertools.chain(definition.get("parameters", ()), common_parameters)
+                    yield self.make_endpoint(full_path, method, parameters, definition)
+        except (KeyError, AttributeError, jsonschema.exceptions.RefResolutionError):
+            raise InvalidSchema("Schema parsing failed. Please check your schema.")
 
     def make_endpoint(
         self, full_path: str, method: str, parameters: Iterator[Dict[str, Any]], definition: Dict[str, Any]
