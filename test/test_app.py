d7a627c218ac927912b533aa823e597ed5836545
Stanislav Tkachenko <stanislav.tkachenko@kiwi.com>
2020-02-14 11:40:56 +0100

    chore(tests): Update platform-specific messages

--- test/test_app.py
+++ test/test_app.py
@@ -1,3 +1,4 @@
+import platform
 import sys
 from pathlib import Path
 
@@ -6,15 +7,20 @@ from _pytest import pytester
 
 HERE = Path(__file__).absolute().parent
 
+if platform.system() == "Windows" and (sys.version_info.major >= 3 and sys.version_info.minor >= 8):
+    AIOHTTP_OUTPUT = "DEBUG:asyncio:Using proactor: IocpProactor"
+else:
+    AIOHTTP_OUTPUT = "DEBUG:asyncio:Using selector:"
+
 
 @pytest.mark.parametrize(
-    "framework, expected", (("flask", "INFO:werkzeug: * Running on"), ("aiohttp", "DEBUG:asyncio:Using selector:"))
+    "framework, expected", (("flask", "INFO:werkzeug: * Running on"), ("aiohttp", AIOHTTP_OUTPUT),),
 )
 def test_app(testdir, aiohttp_unused_port, framework, expected):
     # When the testing app is run from CMD
     port = aiohttp_unused_port()
     with pytest.raises(pytester.Testdir.TimeoutExpired):
-        testdir.run(sys.executable, f"{HERE / 'apps/__init__.py'}", str(port), f"--framework={framework}", timeout=0.75)
+        testdir.run(sys.executable, f"{HERE / 'apps/__init__.py'}", str(port), f"--framework={framework}", timeout=2.0)
     with testdir.tmpdir.join("stderr").open() as fd:
         stderr = fd.read()
     # Then it should start OK and emit debug logs
