schemathesis/schemathesis

commit 04aeda15ceeb5bd89f1d2f90ad6d9c3d4de43e7d

fix: Content Type conformance check for Open API 3.0 schemas (#465)

src/schemathesis/schemas.py
@@ -28,7 +28,7 @@ from .exceptions import InvalidSchema
 from .filters import should_skip_by_tag, should_skip_endpoint, should_skip_method
 from .models import Endpoint, empty_object
 from .types import Filter, Hook, NotSet
-from .utils import NOT_SET, StringDatesYAMLLoader
+from .utils import NOT_SET, GenericResponse, StringDatesYAMLLoader
 
 
 def load_file_impl(location: str, opener: Callable) -> Dict[str, Any]:
@@ -165,6 +165,10 @@ class BaseSchema(Mapping):
     def get_hook(self, place: str) -> Optional[Hook]:
         key = HookLocation[place]
         return self.hooks.get(key)
+
+    def get_content_types(self, endpoint: Endpoint, response: GenericResponse) -> List[str]:
+        """Content types available for this endpoint."""
+        raise NotImplementedError
 
 
 class SwaggerV20(BaseSchema):
@@ -318,6 +322,12 @@ class SwaggerV20(BaseSchema):
     def _get_response_schema(self, definition: Dict[str, Any]) -> Optional[Dict[str, Any]]:
         return definition.get("schema")
 
+    def get_content_types(self, endpoint: Endpoint, response: GenericResponse) -> List[str]:
+        produces = endpoint.definition.get("produces", None)
+        if produces:
+            return produces
+        return self.raw_schema.get("produces", [])
+
 
 class OpenApi30(SwaggerV20):  # pylint: disable=too-many-ancestors
     nullable_name = "nullable"
@@ -398,6 +408,16 @@ class OpenApi30(SwaggerV20):  # pylint: disable=too-many-ancestors
         if option:
             return option["schema"]
         return None
+
+    def get_content_types(self, endpoint: Endpoint, response: GenericResponse) -> List[str]:
+        try:
+            responses = endpoint.definition["responses"]
+        except KeyError:
+            # Possible to get if `validate_schema=False` is passed during schema creation
+            raise InvalidSchema("Schema parsing failed. Please check your schema.")
+        definitions = responses.get(str(response.status_code), {}).values()
+        content_types = [list(definition.keys()) for definition in definitions]
+        return list(itertools.chain(*content_types))
 
 
 def get_common_parameters(methods: Dict[str, Any]) -> List[Dict[str, Any]]:

test/runner/test_checks.py
@@ -11,6 +11,7 @@ from schemathesis.checks import (
     response_schema_conformance,
     status_code_conformance,
 )
+from schemathesis.exceptions import InvalidSchema
 from schemathesis.schemas import BaseSchema
 
 
@@ -28,15 +29,41 @@ def make_response(content=b"{}", content_type="application/json") -> requests.Re
 
 
 @pytest.fixture()
+def spec(request):
+    param = getattr(request, "param", None)
+    if param == "swagger":
+        return request.getfixturevalue("swagger_20")
+    elif param == "openapi":
+        return request.getfixturevalue("openapi_30")
+    return request.getfixturevalue("swagger_20")
+
+
+@pytest.fixture()
 def response(request):
     return make_response(content_type=request.param)
 
 
 @pytest.fixture()
-def case(request, swagger_20) -> models.Case:
-    return make_case(swagger_20, {"produces": request.param})
+def case(request, spec) -> models.Case:
+    if "swagger" in spec.raw_schema:
+        data = {"produces": request.param}
+    else:
+        data = {
+            "responses": {
+                "200": {
+                    "content": {
+                        # There should be a content type in OAS3. But in test below it is omitted
+                        # For simplicity a default value is implemented here
+                        key: {"schema": {"type": "string"}}
+                        for key in request.param or ["application/json"]
+                    }
+                }
+            }
+        }
+    return make_case(spec, data)
 
 
+@pytest.mark.parametrize("spec", ("swagger", "openapi"), indirect=["spec"])
 @pytest.mark.parametrize(
     "response, case",
     (
@@ -46,7 +73,7 @@ def case(request, swagger_20) -> models.Case:
     ),
     indirect=["response", "case"],
 )
-def test_content_type_conformance_valid(response, case):
+def test_content_type_conformance_valid(spec, response, case):
     assert content_type_conformance(response, case) is None
 
 
@@ -111,12 +138,13 @@ def test_status_code_conformance_invalid(value, swagger_20):
     assert exc_info.type.__name__ == f"StatusCodeError{value}"
 
 
+@pytest.mark.parametrize("spec", ("swagger", "openapi"), indirect=["spec"])
 @pytest.mark.parametrize(
     "response, case",
     (("plain/text", ["application/json"]), ("plain/text;charset=utf-8", ["application/json"])),
     indirect=["response", "case"],
 )
-def test_content_type_conformance_invalid(response, case):
+def test_content_type_conformance_invalid(spec, response, case):
     message = (
         f"^Received a response with '{response.headers['Content-Type']}' Content-Type, "
         "but it is not declared in the schema.\n\nDefined content types: application/json$"
@@ -124,6 +152,24 @@ def test_content_type_conformance_invalid(response, case):
     with pytest.raises(AssertionError, match=message) as exc_info:
         content_type_conformance(response, case)
     assert "SchemaValidationError" in exc_info.type.__name__
+
+
+def test_invalid_schema_on_content_type_check():
+    # When schema validation is disabled and it doesn't contain "responses" key
+    schema = schemathesis.from_dict(
+        {
+            "openapi": "3.0.2",
+            "info": {"title": "Test", "description": "Test", "version": "0.1.0"},
+            "paths": {"/users": {"get": {}}},
+        },
+        validate_schema=False,
+    )
+    endpoint = schema.endpoints["/users"]["get"]
+    case = models.Case(endpoint)
+    response = make_response(content_type="application/json")
+    # Then an error should be risen
+    with pytest.raises(InvalidSchema):
+        content_type_conformance(response, case)
 
 
 SUCCESS_SCHEMA = {"type": "object", "properties": {"success": {"type": "boolean"}}, "required": ["success"]}

src/schemathesis/checks.py
@@ -45,14 +45,11 @@ def _expand_responses(responses: Dict[Union[str, int], Any]) -> Generator[int, N
 
 
 def content_type_conformance(response: GenericResponse, case: "Case") -> None:
-    produces = case.endpoint.definition.get("produces", None)
-    global_produces = case.endpoint.schema.raw_schema.get("produces", None)
-    if global_produces and not produces:
-        produces = global_produces
-    if not produces:
+    content_types = case.endpoint.get_content_types(response)
+    if not content_types:
         return
     content_type = response.headers["Content-Type"]
-    for option in produces:
+    for option in content_types:
         if are_content_types_equal(option, content_type):
             return
         expected_main, expected_sub = parse_content_type(option)
@@ -61,7 +58,7 @@ def content_type_conformance(response: GenericResponse, case: "Case") -> None:
     raise exc_class(
         f"Received a response with '{content_type}' Content-Type, "
         f"but it is not declared in the schema.\n\n"
-        f"Defined content types: {', '.join(produces)}"
+        f"Defined content types: {', '.join(content_types)}"
     )
 
 

src/schemathesis/models.py
@@ -215,6 +215,10 @@ class Endpoint:
 
         return get_case_strategy(self)
 
+    def get_content_types(self, response: GenericResponse) -> List[str]:
+        """Content types available for this endpoint."""
+        return self.schema.get_content_types(self, response)
+
 
 class Status(IntEnum):
     """Status of an action or multiple actions."""

commit bb1fde16f6ca05dd68a359985f177320c23cbc89

fix: Error in `OpenApi30.get_content_types` introduced in `04aeda1` (#470)

src/schemathesis/schemas.py
@@ -415,9 +415,8 @@ class OpenApi30(SwaggerV20):  # pylint: disable=too-many-ancestors
         except KeyError:
             # Possible to get if `validate_schema=False` is passed during schema creation
             raise InvalidSchema("Schema parsing failed. Please check your schema.")
-        definitions = responses.get(str(response.status_code), {}).values()
-        content_types = [list(definition.keys()) for definition in definitions]
-        return list(itertools.chain(*content_types))
+        definitions = responses.get(str(response.status_code), {}).get("content", {})
+        return list(definitions.keys())
 
 
 def get_common_parameters(methods: Dict[str, Any]) -> List[Dict[str, Any]]:

test/runner/test_checks.py
@@ -77,15 +77,14 @@ def test_content_type_conformance_valid(spec, response, case):
     assert content_type_conformance(response, case) is None
 
 
-@pytest.mark.parametrize("content_type, is_error", (("application/json", False), ("application/xml", True),))
-def test_global_produces_override(content_type, is_error):
-    # When "produces" is specified on the schema level and on the operation level
-    schema = schemathesis.from_dict(
+@pytest.mark.parametrize(
+    "raw_schema",
+    (
         {
             "swagger": "2.0",
             "info": {"title": "Sample API", "description": "API description in Markdown.", "version": "1.0.0"},
             "host": "api.example.com",
-            "basePath": "/v1",
+            "basePath": "/",
             "schemes": ["https"],
             "produces": ["application/xml"],
             "paths": {
@@ -98,9 +97,29 @@ def test_global_produces_override(content_type, is_error):
                     }
                 }
             },
-        }
-    )
-    endpoint = schema.endpoints["/v1/users"]["get"]
+        },
+        {
+            "openapi": "3.0.2",
+            "info": {"title": "Test", "description": "Test", "version": "0.1.0"},
+            "paths": {
+                "/users": {
+                    "get": {
+                        "responses": {
+                            "200": {
+                                "description": "OK",
+                                "content": {"application/json": {"schema": {"type": "object"}}},
+                            }
+                        },
+                    }
+                }
+            },
+        },
+    ),
+)
+@pytest.mark.parametrize("content_type, is_error", (("application/json", False), ("application/xml", True),))
+def test_content_type_conformance_integration(raw_schema, content_type, is_error):
+    schema = schemathesis.from_dict(raw_schema)
+    endpoint = schema.endpoints["/users"]["get"]
     case = models.Case(endpoint)
     response = make_response(content_type=content_type)
     if not is_error: