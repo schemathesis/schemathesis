6b4cdfe9bc098673afdf508e7bec6508923f6bf3
Dmitry Dygalo <dadygalo@gmail.com>
2020-04-12 17:44:20 +0200

    fix: Open API -> JSON Schema conversion
    It was converting to Draft 7, but it should be Draft 4

--- test/test_converter.py
+++ test/test_converter.py
@@ -16,9 +16,6 @@ from schemathesis import converter
             {"x-nullable": True, "type": "integer", "enum": [1, 2]},
             {"anyOf": [{"type": "integer", "enum": [1, 2]}, {"type": "null"}]},
         ),
-        ({"type": "integer", "minimum": 0, "exclusiveMinimum": True}, {"type": "integer", "exclusiveMinimum": 0}),
-        ({"type": "integer", "maximum": 0, "exclusiveMaximum": True}, {"type": "integer", "exclusiveMaximum": 0}),
-        ({"type": "integer", "maximum": 0, "exclusiveMaximum": False}, {"type": "integer", "maximum": 0}),
     ),
 )
 def test_to_jsonschema(schema, expected):
