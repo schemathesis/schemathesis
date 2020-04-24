4e8f62084c3964bd2a7b9ef4a405bead1944f2bc
Dmitry Dygalo <dadygalo@gmail.com>
2020-04-24 10:07:47 +0200

    chore: Shutdown writer thread in case of exceptions in the main thread

--- test/cli/test_cassettes.py
+++ test/cli/test_cassettes.py
@@ -7,15 +7,23 @@ from _pytest.main import ExitCode
 from schemathesis.cli.cassettes import get_command_representation
 
 
+@pytest.fixture()
+def cassette_path(tmp_path):
+    return tmp_path / "output.yaml"
+
+
+def load_cassette(path):
+    with path.open() as fd:
+        return yaml.safe_load(fd)
+
+
 @pytest.mark.endpoints("success", "upload_file")
-def test_store_cassette(cli, schema_url, tmp_path):
-    cassette_path = tmp_path / "output.yaml"
+def test_store_cassette(cli, schema_url, cassette_path):
     result = cli.run(
         schema_url, f"--store-network-log={cassette_path}", "--hypothesis-max-examples=2", "--hypothesis-seed=1"
     )
     assert result.exit_code == ExitCode.OK
-    with cassette_path.open() as fd:
-        cassette = yaml.safe_load(fd)
+    cassette = load_cassette(cassette_path)
     assert len(cassette["http_interactions"]) == 3
     assert cassette["http_interactions"][0]["id"] == "0"
     assert cassette["http_interactions"][1]["id"] == "1"
@@ -36,15 +44,27 @@ def test_get_command_representation(mocker):
 
 
 @pytest.mark.endpoints("success")
-def test_run_subprocess(testdir, tmp_path, schema_url):
-    cassette_path = tmp_path / "output.yaml"
+def test_run_subprocess(testdir, cassette_path, schema_url):
     result = testdir.run(
         "schemathesis", "run", f"--store-network-log={cassette_path}", "--hypothesis-max-examples=2", schema_url
     )
     assert result.ret == ExitCode.OK
     assert result.outlines[17] == f"Network log: {cassette_path}"
-    with cassette_path.open() as fd:
-        cassette = yaml.safe_load(fd)
+    cassette = load_cassette(cassette_path)
     assert len(cassette["http_interactions"]) == 1
     command = f"schemathesis run --store-network-log={cassette_path} --hypothesis-max-examples=2 {schema_url}"
     assert cassette["command"] == command
+
+
+@pytest.mark.endpoints("invalid")
+def test_main_process_error(cli, schema_url, cassette_path):
+    # When there is an error in the main process before the background writer is finished
+    # Here it is happening because the schema is not valid
+    result = cli.run(
+        schema_url, f"--store-network-log={cassette_path}", "--hypothesis-max-examples=1", "--hypothesis-seed=1"
+    )
+    assert result.exit_code == ExitCode.TESTS_FAILED
+    # Then there should be no hanging threads
+    # And no cassette
+    cassette = load_cassette(cassette_path)
+    assert cassette is None
