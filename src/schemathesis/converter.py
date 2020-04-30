8cca6c1d1f4d5b2ce1b887cd27757a531269344d
Dmitry Dygalo <dadygalo@gmail.com>
2020-04-30 16:37:11 +0200

    fix: Validation of nullable properties in `response_schema_conformance` check introduced in `1.3.0`

--- src/schemathesis/converter.py
+++ src/schemathesis/converter.py
@@ -1,11 +1,12 @@
 from copy import deepcopy
-from typing import Any, Dict
+from typing import Any, Dict, List, Union, overload
 
 
 def to_json_schema(schema: Dict[str, Any], nullable_name: str) -> Dict[str, Any]:
     """Convert Open API parameters to JSON Schema.
 
-    NOTE. This function is applied to all keywords (including nested) during schema resolving, thus it is not recursive
+    NOTE. This function is applied to all keywords (including nested) during schema resolving, thus it is not recursive.
+    See a recursive version below.
     """
     schema = deepcopy(schema)
     if schema.get(nullable_name) is True:
@@ -21,4 +22,43 @@ def to_json_schema(schema: Dict[str, Any], nullable_name: str) -> Dict[str, Any]
     if schema.get("type") == "file":
         schema["type"] = "string"
         schema["format"] = "binary"
+    return schema
+
+
+Schema = Union[Dict[str, Any], List, str, float, int]
+
+
+@overload
+def to_json_schema_recursive(schema: Dict[str, Any], nullable_name: str) -> Dict[str, Any]:
+    pass
+
+
+@overload
+def to_json_schema_recursive(schema: List, nullable_name: str) -> List:
+    pass
+
+
+@overload
+def to_json_schema_recursive(schema: str, nullable_name: str) -> str:
+    pass
+
+
+@overload
+def to_json_schema_recursive(schema: float, nullable_name: str) -> float:
+    pass
+
+
+def to_json_schema_recursive(schema: Schema, nullable_name: str) -> Schema:
+    """Apply ``to_json_schema`` recursively.
+
+    This version is needed for cases where the input schema was not resolved and ``to_json_schema`` wasn't applied
+    recursively.
+    """
+    if isinstance(schema, dict):
+        schema = to_json_schema(schema, nullable_name)
+        for key, sub_item in schema.items():
+            schema[key] = to_json_schema_recursive(sub_item, nullable_name)
+    elif isinstance(schema, list):
+        for idx, sub_item in enumerate(schema):
+            schema[idx] = to_json_schema_recursive(sub_item, nullable_name)
     return schema
