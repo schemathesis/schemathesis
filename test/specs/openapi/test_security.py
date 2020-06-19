932876567df5cba7d4475b3777100ae03a22502d
Dmitry Dygalo <dadygalo@gmail.com>
2020-06-19 19:06:04 +0200

    test: Add tests for some not covered code path

---
+++ test/specs/openapi/test_security.py
@@ -0,0 +1,15 @@
+from schemathesis.specs.openapi.references import ConvertingResolver
+from schemathesis.specs.openapi.security import OpenAPISecurityProcessor
+
+
+def test_ref_resolving():
+    http_schema = {"basic_auth": {"type": "http", "scheme": "basic"}}
+    schema = {
+        "openapi": "3.0.0",
+        "info": {"title": "Blank API", "version": "1.0"},
+        "servers": [{"url": "http://localhost/api"}],
+        "paths": {"foo": {"get": {"responses": {"200": {"description": "OK"}},}}},
+        "components": {"securitySchemes": {"$ref": "#/components/HTTPSchema"}, "HTTPSchema": http_schema},
+    }
+    resolver = ConvertingResolver("", schema, nullable_name="nullable")
+    assert OpenAPISecurityProcessor().get_security_definitions(schema, resolver) == http_schema
