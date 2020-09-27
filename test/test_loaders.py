e33d779d88ef0a7bf9fc085df478a8cc71ae09d4
Tin Lam <tin@irrational.io>
2020-09-27 06:14:12 -0500

    fix(uri): allows specifying port for base_url
    This patch set adds a new port parameter to .from_uri() when a base_url
    is not specified but a port is, it will take the original URL and adjust
    the port to the specified one.
    
    Closes: #706
    
    Signed-off-by: Tin Lam <tin@irrational.io>

--- test/test_loaders.py
+++ test/test_loaders.py
@@ -1,6 +1,8 @@
 import json
 
 import pytest
+from requests.models import Response
+from yarl import URL
 
 import schemathesis
 from schemathesis.constants import USER_AGENT
@@ -36,6 +38,27 @@ def test_base_url_override(openapi3_schema_url, url):
     schema = schemathesis.from_uri(openapi3_schema_url, base_url=url)
     endpoint = next(schema.get_all_endpoints())
     assert endpoint.base_url == "http://example.com"
+
+
+def test_port_override(openapi3_schema_url):
+    schema = schemathesis.from_uri(openapi3_schema_url, port=8081)
+    endpoint = next(schema.get_all_endpoints())
+    assert endpoint.base_url == "http://127.0.0.1:8081/schema.yaml"
+
+
+def test_port_override_with_ipv6(openapi3_schema_url, simple_openapi, mocker):
+    resp = Response()
+    resp.status_code = 200
+    resp._content = json.dumps(simple_openapi).encode("utf-8")
+    mocker.patch("requests.get", return_value=resp)
+
+    url = URL(openapi3_schema_url)
+    parts = list(map(int, url.host.split(".")))
+    ipv6_host = "2002:{:02x}{:02x}:{:02x}{:02x}::".format(*parts)
+    ipv6 = url.with_host("[%s]" % ipv6_host)
+    schema = schemathesis.from_uri(str(ipv6), validate_schema=False, port=8081)
+    endpoint = next(schema.get_all_endpoints())
+    assert endpoint.base_url == "http://[2002:7f00:1::]:8081/schema.yaml"
 
 
 def test_unsupported_type():
