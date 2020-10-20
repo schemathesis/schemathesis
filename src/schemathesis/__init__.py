31ec2aa917b96dfa2e13616b6acaa2870e235f08
Dmitry Dygalo <dadygalo@gmail.com>
2020-10-20 13:51:35 +0200

    chore: Export `from_asgi` loader on the top-level package

--- src/schemathesis/__init__.py
+++ src/schemathesis/__init__.py
@@ -1,7 +1,7 @@
 from . import fixups, hooks, targets
 from .cli import register_check, register_target
 from .constants import __version__
-from .loaders import from_dict, from_file, from_path, from_pytest_fixture, from_uri, from_wsgi
+from .loaders import from_asgi, from_dict, from_file, from_path, from_pytest_fixture, from_uri, from_wsgi
 from .models import Case
 from .specs import graphql
 from .specs.openapi._hypothesis import init_default_strategies, register_string_format
