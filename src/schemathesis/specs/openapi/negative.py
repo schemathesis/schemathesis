0e71d3ad10a2d244eeebbc7dcfeb51efab6c4641
Dmitry Dygalo <dadygalo@gmail.com>
2020-11-04 00:05:16 +0100

    chore: Improve schema negating

--- src/schemathesis/specs/openapi/negative.py
+++ src/schemathesis/specs/openapi/negative.py
@@ -1,6 +1,8 @@
 from copy import deepcopy
-from typing import Any, Callable, Dict, TypeVar
+from typing import Any, Callable, Dict, List, Set, Tuple, TypeVar
 
+import attr
+import jsonschema
 from hypothesis import strategies as st
 from hypothesis.strategies._internal.featureflags import FeatureStrategy
 from hypothesis_jsonschema import from_schema
@@ -8,55 +10,106 @@ from hypothesis_jsonschema import from_schema
 T = TypeVar("T")
 Draw = Callable[[st.SearchStrategy[T]], T]
 Schema = Dict[str, Any]
-# draw, new_schema, property schema, name
-Mutation = Callable[[Draw, Schema, Schema, str], None]
 
 
-def negative_schema(schema: Dict[str, Any]) -> st.SearchStrategy:
-    """A strategy for objects, that DO NOT match the input schema."""
+@attr.s(slots=True)
+class Mutation:
+    def mutate(self, draw: Draw, new_schema: Schema, property_schema: Schema, name: str) -> None:
+        raise NotImplementedError
 
+
+@attr.s(slots=True)
+class MutateRequired(Mutation):
+    def mutate(self, draw: Draw, new_schema: Schema, property_schema: Schema, name: str) -> None:
+        """Make the schema never generate the given property."""
+        required = new_schema.get("required")
+        if required and name in required:
+            required.remove(name)
+        # An optional property still can be generated
+        # To avoid it we need to remove it completely
+        properties = new_schema["properties"]
+        del properties[name]
+
+
+@attr.s(slots=True)
+class MutateType(Mutation):
+    possible_types: Set[str] = attr.ib(
+        factory=lambda: {"string", "integer", "number", "object", "array", "boolean", "null"}
+    )
+
+    def mutate(self, draw: Draw, new_schema: Schema, property_schema: Schema, name: str) -> None:
+        """Change the property type if possible."""
+        to_exclude = set(get_type(property_schema))
+        available_types = self.possible_types - to_exclude
+        if available_types:
+            property_schema["type"] = draw(st.sampled_from(sorted(available_types)))
+
+
+MUTATIONS = {
+    "headers": (MutateRequired(), MutateType()),
+    "cookies": (MutateRequired(), MutateType()),
+    # We can't mutate `required` since it is required by the spec to be valid.
+    # I.e. if any of path parameters is absent, then we can't reach the endpoint
+    "path_parameters": (MutateType(),),
+    "query": (MutateRequired(), MutateType()),
+    "body": (MutateRequired(), MutateType()),
+    "form_data": (MutateRequired(), MutateType()),
+}
+
+
+# Thoughts:
+#   - Consider a request mutated when at least one parameter is mutated. I.e. one from "header", "query", etc.
+#     Some parameters can't be mutated, but it should matter only when nothing at all can't be mutated.
+
+
+def negative_schema(
+    schema: Dict[str, Any], parameter: str, *, custom_formats: Dict[str, st.SearchStrategy[str]]
+) -> st.SearchStrategy:
+    """A strategy for instances, that DO NOT match the input schema."""
+    mutations = MUTATIONS[parameter]
+
+    validator = jsonschema.Draft4Validator(schema)
     # First we mutate the input schema, so any valid instance for it is invalid to the original schema
 
     @st.composite  # type: ignore
-    def inner(draw: Draw) -> Any:
+    def mutated(draw: Draw) -> Any:
         new_schema = deepcopy(schema)
-        properties = sorted(new_schema.get("properties", {}).items())
-        # At least one mutation should be done to at least one property
-        mutated_property_name, mutated_property_schema = draw(st.sampled_from(properties))
-        mutation = draw(st.sampled_from(MUTATIONS))
-        mutation(draw, new_schema, mutated_property_schema, mutated_property_name)
+        # Schemathesis creates "object" schemas for all parameter "in" values except "body" (which is taken as is).
+        # In OAS 2 the "type" keyword is a list (in OAS 3 it is a string) therefore we need to convert "type" to a list
+        if "object" in get_type(new_schema):
+            return mutate_object_schema(draw, new_schema, mutations)
+        return {"not": new_schema}
 
-        # Other properties & mutations are chosen with feature flags
-        enabled_properties = draw(FeatureStrategy())
-        enabled_mutations = draw(st.shared(FeatureStrategy(), "enabled_mutations"))
-
-        for name, property_schema in properties:
-            if name != mutated_property_name and enabled_properties.is_enabled(name):
-                for mutation in MUTATIONS:
-                    if enabled_mutations.is_enabled(mutation.__name__):
-                        mutation(draw, new_schema, property_schema, name)
-
-        return new_schema
-
-    return inner().flatmap(from_schema)
+    return (
+        mutated()
+        .flatmap(lambda s: from_schema(s, custom_formats=custom_formats))
+        .filter(lambda i: not validator.is_valid(i))
+    )
 
 
-def mutate_required(draw: Draw, new_schema: Schema, property_schema: Schema, name: str) -> None:
-    required = new_schema.get("required")
-    if required and name in required:
-        required.remove(name)
-    # An optional property still can be generated
-    # To avoid it we need to remove it completely
-    properties = new_schema["properties"]
-    del properties[name]
+def get_type(schema: Schema) -> List[str]:
+    type_ = schema.get("type", ["null", "boolean", "integer", "number", "string", "array", "object"])
+    if isinstance(type_, str):
+        return [type_]
+    return type_
 
 
-def mutate_type(draw: Draw, new_schema: Schema, property_schema: Schema, name: str) -> None:
-    property_type = property_schema["type"]
-    to_exclude = set(property_type) if isinstance(property_type, list) else {property_type}
-    available_types = {"string", "integer", "number", "object", "array", "boolean", "null"} - to_exclude
-    if available_types:
-        property_schema["type"] = draw(st.sampled_from(sorted(available_types)))
+def mutate_object_schema(draw: Draw, schema: Schema, mutations: Tuple[Mutation, ...]) -> Schema:
+    properties = sorted(schema.get("properties", {}).items())
+    if not properties:
+        return {"not": schema}
+    # At least one mutation should be done to at least one property
+    mutated_property_name, mutated_property_schema = draw(st.sampled_from(properties))
+    mutation = draw(st.sampled_from(mutations))
+    mutation.mutate(draw, schema, mutated_property_schema, mutated_property_name)
 
+    # Other properties & mutations are chosen with feature flags
+    features = draw(st.shared(FeatureStrategy(), key="features"))
 
-MUTATIONS = [mutate_required, mutate_type]
+    for name, property_schema in properties:
+        if name != mutated_property_name and features.is_enabled(name):
+            for mutation in mutations:
+                if features.is_enabled(mutation.__class__.__name__):
+                    mutation.mutate(draw, schema, property_schema, name)
+
+    return schema
--- test/specs/openapi/test_negative.py
+++ test/specs/openapi/test_negative.py
@@ -1,15 +1,17 @@
-from hypothesis import given
+import pytest
+from hypothesis import given, settings
 from jsonschema import Draft4Validator
 
 from schemathesis.specs.openapi.negative import negative_schema
 
-EXAMPLE = {
+OBJECT_SCHEMA = {
     "type": "object",
     "properties": {
         "foo": {"type": "string"},
         "bar": {"type": "integer"},
         "baf": {"type": ["integer"]},
         "baz": {"type": ["array", "object"]},
+        "bad": {},
     },
     "required": [
         "foo",
@@ -18,12 +20,21 @@ EXAMPLE = {
         "baz",
     ],
 }
+ARRAY_SCHEMA = {"type": "array", "items": OBJECT_SCHEMA}
+EMPTY_OBJECT_SCHEMA = {
+    "type": "object",
+}
+INTEGER_SCHEMA = {
+    "type": "integer",
+}
 
 
-def test_negative():
-    validator = Draft4Validator(EXAMPLE)
+@pytest.mark.parametrize("schema", (OBJECT_SCHEMA, EMPTY_OBJECT_SCHEMA, ARRAY_SCHEMA, INTEGER_SCHEMA))
+def test_negative(schema):
+    validator = Draft4Validator(schema)
 
-    @given(negative_schema(EXAMPLE))
+    @given(negative_schema(schema, parameter="query", custom_formats={}))
+    @settings(max_examples=10)
     def test(instance):
         assert not validator.is_valid(instance)
 
