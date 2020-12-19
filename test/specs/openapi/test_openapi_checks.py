059fbfed0aefd221c7caacf8c8ef50bf8943d189
Dmitry Dygalo <dadygalo@gmail.com>
2020-12-19 20:09:57 +0100

    test: Add more tests

--- test/specs/openapi/test_openapi_checks.py
+++ test/specs/openapi/test_openapi_checks.py
@@ -4,10 +4,18 @@ import requests
 from schemathesis import Case
 from schemathesis.models import Endpoint
 from schemathesis.schemas import BaseSchema
-from schemathesis.specs.openapi.checks import content_type_conformance, response_schema_conformance
+from schemathesis.specs.openapi.checks import (
+    content_type_conformance,
+    response_headers_conformance,
+    response_schema_conformance,
+    status_code_conformance,
+)
 
 
-@pytest.mark.parametrize("check", (content_type_conformance, response_schema_conformance))
+@pytest.mark.parametrize(
+    "check",
+    (content_type_conformance, response_headers_conformance, response_schema_conformance, status_code_conformance),
+)
 def test_wrong_schema_type(check):
     schema = BaseSchema({})
     # These checks should not be valid for some generic schema
