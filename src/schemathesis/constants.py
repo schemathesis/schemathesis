76f42cb3ab457a71d239d8bf2287b3e18178e7e9
Dmitry Dygalo <dadygalo@gmail.com>
2021-01-05 18:31:11 +0100

    chore: Improve error messages for serialization errors

--- src/schemathesis/constants.py
+++ src/schemathesis/constants.py
@@ -17,6 +17,11 @@ RECURSIVE_REFERENCE_ERROR_MESSAGE = (
     "recursive references in the operation definition. See more information in "
     "this issue - https://github.com/schemathesis/schemathesis/issues/947"
 )
+SERIALIZERS_SUGGESTION_MESSAGE = (
+    "You can register your own serializer with `schemathesis.serializers.register` "
+    "and Schemathesis will be able to make API calls with this media type. \n"
+    "See https://schemathesis.readthedocs.io/en/stable/how.html#payload-serialization for more information."
+)
 
 
 class DataGenerationMethod(str, Enum):
