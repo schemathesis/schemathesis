21072aecc8f7411d3abe9975328e1cad2141f31c
Dmitry Dygalo <dadygalo@gmail.com>
2021-01-12 20:49:22 +0100

    chore: Do not export `Stateful` in the public API

--- src/schemathesis/__init__.py
+++ src/schemathesis/__init__.py
@@ -5,7 +5,6 @@ from .loaders import from_asgi, from_dict, from_file, from_path, from_pytest_fix
 from .models import Case
 from .specs import graphql
 from .specs.openapi._hypothesis import init_default_strategies, register_string_format
-from .stateful import Stateful
 from .utils import GenericResponse
 
 init_default_strategies()
