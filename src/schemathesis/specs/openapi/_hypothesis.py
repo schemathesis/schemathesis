31d322fc4030dfbe3bcef7130446d8a1b4804fa3
Dmitry Dygalo <dadygalo@gmail.com>
2021-05-31 20:16:05 +0200

    fix: Too much filtering in headers that have schemas with the `pattern` keyword
    Ref: #1189

--- src/schemathesis/specs/openapi/_hypothesis.py
+++ src/schemathesis/specs/openapi/_hypothesis.py
@@ -285,7 +285,8 @@ def make_positive_strategy(schema: Dict[str, Any], location: str) -> st.SearchSt
     if is_header_location(location):
         # We try to enforce the right header values via "format"
         # This way, only allowed values will be used during data generation, which reduces the amount of filtering later
-        set_keyword_on_properties(schema, "format", "_header_value")
+        # If a property schema contains `pattern` it leads to heavy filtering and worse performance - therefore, skip it
+        set_keyword_on_properties(schema, "format", "_header_value", lambda s: "pattern" not in s)
     return from_schema(schema, custom_formats=STRING_FORMATS)
 
 
