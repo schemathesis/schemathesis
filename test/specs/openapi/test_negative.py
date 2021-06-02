1308261600dd83d3b809a28c6271d26267e01f28
Dmitry Dygalo <dadygalo@gmail.com>
2021-06-02 21:04:33 +0200

    test: Disable deadline

--- test/specs/openapi/test_negative.py
+++ test/specs/openapi/test_negative.py
@@ -194,7 +194,7 @@ def test_successful_mutations(data, mutation, schema):
     ),
 )
 @given(data=st.data())
-@settings(suppress_health_check=[HealthCheck.too_slow, HealthCheck.filter_too_much])
+@settings(deadline=None, suppress_health_check=[HealthCheck.too_slow, HealthCheck.filter_too_much])
 def test_path_parameters_are_string(data, schema):
     validator = Draft4Validator(schema)
     new_schema = deepcopy(schema)
