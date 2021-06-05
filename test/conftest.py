250c20ef4f68187d0644458fe73fabaf7858b4bc
Dmitry Dygalo <dadygalo@gmail.com>
2021-06-05 16:29:32 +0200

    chore: Avoid ``pytest`warnings when internal Schemathesis classes are in the test module scope

--- test/conftest.py
+++ test/conftest.py
@@ -530,11 +530,11 @@ def testdir(testdir):
 
         note = print  # An alias to distinguish with leftover prints
 
-        schema = schemathesis.from_dict(raw_schema, method={method}, endpoint={endpoint}, tag={tag}, validate_schema={validate_schema})
-
         @pytest.fixture
         def simple_schema():
             return schema
+
+        schema = schemathesis.from_dict(raw_schema, method={method}, endpoint={endpoint}, tag={tag}, validate_schema={validate_schema})
         """.format(
                 schema=schema,
                 method=repr(method),
