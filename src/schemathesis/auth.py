74c01663c0874f5102106b05198079752c832842
JangasCodingplace <janisgoesser92@gmail.com>
2021-10-06 01:43:22 +0200

    feature/966 init get token from api method

--- src/schemathesis/auth.py
+++ src/schemathesis/auth.py
@@ -1,93 +1,82 @@
 import requests
-from enum import Enum
-from typing import Optional, Any
+from datetime import datetime
+from typing import Optional, Any, Callable, Dict
 from functools import partial
 from requests.auth import HTTPBasicAuth
 
 
-class AuthType(Enum):
-    COOKIE = 0
-    HEADER = 1
-    QUERY_PARAMS = 2
-    USERNAME_PASSWORD = 3
+USERNAME: str = ""      # TODO: get value from global state
+PASSWORD: str = ""      # TODO: get value from global state
 
 
-class AuthStorage:
-    """Description
+class AuthToken:
+    def __init__(self, token_value: str, timestamp: Optional[datetime] = None):
+        self.value = token_value    # TODO: Map this reference to initialy set token value
+        self.timestamp = timestamp if timestamp else datetime.now()
 
-    Szenarios:
-        No Token/Auth Set for request yet
-        Token Expires
 
-    Auth Methods
-        Cookie
-        Header
-        request auth
+def perform_api_authentication(url: str, payload: Dict["str", "str"]) -> requests.Response:
+    response = requests.request(
+        method="POST",
+        url=url,
+        payload=payload
+    )
+    response.raise_for_status()
+    return response
 
-    Keyword Arguments:
-        username
-        password
-        auth_token
-    """
 
+class APITokenAuth:
     login_url: Optional[str] = None
-    refresh_url: Optional[str] = None
-    auth_type: AuthType = AuthType.HEADER
-    auth_callback: Optional[Any] = None
-
-    DEFAULT_CONFIG = {
-        "username": None,
-        "password": None,
-        "token": None
-    }
+    perform_authentication: Callable = perform_api_authentication
+    token_kwarg_name = "token"
 
     def __init__(self, **configs):
-        if self.auth_type == AuthType.HEADER:
-            pass
-        elif self.auth_type == AuthType.COOKIE:
-            pass
-        elif self.auth_type == AuthType.QUERY_PARAMS:
-            pass
-        elif self.auth_type == AuthType.USERNAME_PASSWORD:
-            self._username = configs.get("username")
-            self._password = configs.get("password")
-        else:
-            raise TypeError("auth_type not supported.")
+        self._username = configs.get("username", USERNAME)
+        self._password = configs.get("password", PASSWORD)
 
-    def _perform_username_password_authentication(self):
-        """Global override of request.request method with auth callback
-
-        Requirement: Each authentication needs to be valid on adding username / password combination
-            per request
-        """
-        if not self.auth_callback:
-            # Warning: Overriding static param will have singleton side effect
-            self.auth_callback = HTTPBasicAuth
-        requests.request = partial(
-            requests.request,
-            auth=self.auth_callback(
-                username=self._username,
-                password=self._password
-            )
-        )
-
-    def perform_authentication(self):
-        if self.auth_type == AuthType.HEADER:
-            pass
-        elif self.auth_type == AuthType.COOKIE:
-            pass
-        elif self.auth_type == AuthType.QUERY_PARAMS:
-            pass
-        elif self.auth_type == AuthType.USERNAME_PASSWORD:
-            self._perform_username_password_authentication()
-        else:
-            raise TypeError("auth_type not supported.")
+    def get_token(self) -> str:
+        payload = {"username": self._username, "password": self._password}
+        auth_response = self.perform_authentication(url=self.login_url, payload=payload)
+        response_data = auth_response.json()
+        return response_data[self.token_kwarg_name]
 
 
-def register_auth_provider(auto_refresh: bool = True, refresh_interval: Optional[int] = None):
+class AuthStorage(APITokenAuth):
+    auth_token: Optional[AuthToken] = None  # Warning: this has a singleton like effect. If this is override,
+                                            # it will be overriden global for all references
+
+    def __init__(self, **configs):
+        super().__init__(**configs)
+
+    def override_token(self):
+        token = self.get_token()
+        self.auth = AuthToken(token_value=token)
+
+
+def register_auth_provider(
+        auth_storage: AuthStorage,
+        auto_refresh: bool = True,
+        refresh_interval: Optional[int] = None,
+        *args,
+        **kwargs
+):
     """
     Programflow:
     1. Check if refresh interval is set - if yes, check if auth is expired
     2. Check if auto refresh is set. If Yes: authenticate user per request
     """
-    pass
+    # TODO: Pass arguments in AuthStorage
+    auth = AuthStorage()
+    if auto_refresh:
+        auth.override_token()
+    if refresh_interval:
+        time_diff = datetime.now() - auth.auth_token.timestamp
+        if time_diff.total_seconds() > refresh_interval:
+            auth.override_token()
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            r = func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
