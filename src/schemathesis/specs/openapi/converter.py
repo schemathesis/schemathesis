9334ef3e1d09b34787e62d9894f6d8b52261b81a
Dmitry Dygalo <dmitry@dygalo.dev>
2021-10-31 16:47:00 +0100

    perf: Avoid `copy.deepcopy` calls in some cases

--- src/schemathesis/specs/openapi/converter.py
+++ src/schemathesis/specs/openapi/converter.py
@@ -4,13 +4,14 @@ from typing import Any, Dict
 from ...utils import traverse_schema
 
 
-def to_json_schema(schema: Dict[str, Any], nullable_name: str) -> Dict[str, Any]:
+def to_json_schema(schema: Dict[str, Any], *, nullable_name: str, copy: bool = True) -> Dict[str, Any]:
     """Convert Open API parameters to JSON Schema.
 
     NOTE. This function is applied to all keywords (including nested) during a schema resolving, thus it is not recursive.
     See a recursive version below.
     """
-    schema = deepcopy(schema)
+    if copy:
+        schema = deepcopy(schema)
     if schema.get(nullable_name) is True:
         del schema[nullable_name]
         schema = {"anyOf": [schema, {"type": "null"}]}
@@ -21,4 +22,4 @@ def to_json_schema(schema: Dict[str, Any], nullable_name: str) -> Dict[str, Any]
 
 
 def to_json_schema_recursive(schema: Dict[str, Any], nullable_name: str) -> Dict[str, Any]:
-    return traverse_schema(schema, to_json_schema, nullable_name)
+    return traverse_schema(schema, to_json_schema, nullable_name=nullable_name)
