7a2b0860d6e775dbe4aa5f91eb05c4b452a15f18
Dmitry Dygalo <dmitry@dygalo.dev>
2022-03-25 12:22:32 +0100

    fix: Create auth file even if some parent dir does not exist

--- test/service/test_auth.py
+++ test/service/test_auth.py
@@ -24,7 +24,7 @@ def assert_token(hosts_file, token):
     ),
     ids=["nothing", "malformed-file"],
 )
-def test_auth_with_explicit_token(cli, hosts_file, setup):
+def test_explicit_token(cli, hosts_file, setup):
     setup(hosts_file)
     # When the user logs in with a token
     token = "sample_token"
@@ -34,11 +34,22 @@ def test_auth_with_explicit_token(cli, hosts_file, setup):
     assert_token(hosts_file, token)
 
 
-def test_auth_with_env_var(cli, hosts_file, monkeypatch):
+def test_env_var(cli, hosts_file, monkeypatch):
     token = "sample_token"
     monkeypatch.setenv(TOKEN_ENV_VAR, token)
     # When the user logs in with an env var token
     result = cli.auth.login(f"--hosts-file={hosts_file}")
     # Then the command succeeds
+    assert result.exit_code == 0, result.stdout
+    assert_token(hosts_file, token)
+
+
+def test_missing_parent_dir(cli, tmp_path):
+    # When the config directory does not exist
+    # And its parent dir does not exist as well
+    hosts_file = tmp_path / "a" / "b" / "hosts.toml"
+    token = "sample_token"
+    result = cli.auth.login(token, f"--hosts-file={hosts_file}")
+    # Then the config file should be created anyway
     assert result.exit_code == 0, result.stdout
     assert_token(hosts_file, token)
