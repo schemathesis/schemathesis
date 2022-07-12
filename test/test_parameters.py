8d8e9d9f718af37834a1acbd62f4b421d24dbbf8
Dmitry Dygalo <dmitry@dygalo.dev>
2022-07-12 13:17:55 +0200

    fix: Generating invalid headers when their schema has `array` or `object` types.

--- test/test_parameters.py
+++ test/test_parameters.py
@@ -42,7 +42,7 @@ def test_cookies(testdir, type_):
     testdir.make_test(
         """
 @schema.parametrize()
-@settings(suppress_health_check=[HealthCheck.filter_too_much], deadline=None, max_examples=3)
+@settings(suppress_health_check=[HealthCheck.filter_too_much], deadline=None, max_examples=20)
 def test_(case):
     assert_str(case.cookies["token"])
     assert_requests_call(case)
