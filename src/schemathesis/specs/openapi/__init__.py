8fb4227d8b0d4b4821769f81c660de6f097f4e64
Dmitry Dygalo <dmitry@dygalo.dev>
2023-01-08 13:22:43 +0100

    chore: Add `schemathesis.openapi.format`

--- src/schemathesis/specs/openapi/__init__.py
+++ src/schemathesis/specs/openapi/__init__.py
@@ -1,1 +1,2 @@
+from ._hypothesis import register_string_format as format  # pylint: disable=redefined-builtin
 from .loaders import from_aiohttp, from_asgi, from_dict, from_file, from_path, from_pytest_fixture, from_uri, from_wsgi
