1bd3f99b4662b9c949f766aa539710f3bbeb6b17
Dmitry Dygalo <dmitry@dygalo.dev>
2023-02-27 14:49:24 +0100

    fix: `Unsatisfiable` error when explicit header name passed via CLI clashes with the header parameter name
    Ref: #1699

--- test/specs/openapi/test_security.py
+++ test/specs/openapi/test_security.py
@@ -1,3 +1,7 @@
+import pytest
+from hypothesis import given
+
+import schemathesis
 from schemathesis.specs.openapi.references import InliningResolver
 from schemathesis.specs.openapi.security import OpenAPISecurityProcessor
 
@@ -13,3 +17,49 @@ def test_ref_resolving():
     }
     resolver = InliningResolver("", schema)
     assert OpenAPISecurityProcessor().get_security_definitions(schema, resolver) == http_schema
+
+
+PARAMETER_NAME = "TestApiKey"
+SCHEMA_WITH_PARAMETER_AND_SECURITY_SCHEME = {
+    "openapi": "3.0.2",
+    "info": {"title": "Test", "description": "Test", "version": "0.1.0"},
+    "paths": {
+        "/test": {
+            "parameters": [
+                {
+                    "name": PARAMETER_NAME,
+                    "in": "header",
+                    "required": True,
+                    "schema": {"enum": ["EXPECTED"]},
+                }
+            ],
+            "get": {"responses": {"200": {"description": "OK"}}},
+        }
+    },
+    "components": {
+        "securitySchemes": {
+            "ApiKeyAuth": {"type": "apiKey", "name": PARAMETER_NAME, "in": "header"},
+            "BasicAuth": {"type": "http", "scheme": "basic"},
+        }
+    },
+    "security": [{"ApiKeyAuth": []}, {"BasicAuth": []}],
+}
+
+
+@pytest.mark.parametrize(
+    "kwargs, expected",
+    (
+        ({}, "EXPECTED"),
+        ({"headers": {"TestApiKey": "EXPLICIT"}}, "EXPLICIT"),
+    ),
+)
+def test_name_clash(kwargs, expected):
+    # Operation definition should take precedence over security schemes
+    # Explicit headers should take precedence over everything else
+    schema = schemathesis.from_dict(SCHEMA_WITH_PARAMETER_AND_SECURITY_SCHEME, validate_schema=True)
+
+    @given(case=schema["/test"]["GET"].as_strategy(**kwargs))
+    def test(case):
+        assert case.headers[PARAMETER_NAME] == expected
+
+    test()
