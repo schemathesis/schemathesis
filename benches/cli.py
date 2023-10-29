075dfdb6741e054fa3c3b0896aea25f5900984a2
Dmitry Dygalo <dmitry@dygalo.dev>
2023-10-29 12:48:37 +0100

    perf: add benchmarks for CLI startup

---
+++ benches/cli.py
@@ -0,0 +1,18 @@
+import sys
+import pytest
+
+pytest_plugins = ["pytester"]
+original = list(sys.modules)
+
+
+@pytest.mark.benchmark
+def test_cli_startup(testdir):
+    # Measure the import time because running via subprocess does not give proper benchmark results under codspeed
+
+    import schemathesis.cli
+
+    for key in list(sys.modules):
+        # PyO3 modules can't be initialized multiple times
+        if key != "rpds" and key not in original:
+            del sys.modules[key]
+    del schemathesis
