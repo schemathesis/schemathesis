83c6e8390b0dedbfa50739e30082a1a70fd3ae6f
Dmitry Dygalo <dmitry@dygalo.dev>
2023-11-28 00:06:42 +0100

    test: Simplify `test_app`

--- test/test_app.py
+++ test/test_app.py
@@ -18,20 +18,12 @@ else:
 
 HERE = Path(__file__).absolute().parent
 
-if platform.system() == "Windows" and (sys.version_info.major >= 3 and sys.version_info.minor >= 8):
-    AIOHTTP_OUTPUT = "DEBUG:asyncio:Using proactor: IocpProactor"
-else:
-    AIOHTTP_OUTPUT = "DEBUG:asyncio:Using selector:"
-
 
 @pytest.mark.parametrize(
-    "framework, expected",
-    (
-        ("flask", "INFO:werkzeug:"),
-        ("aiohttp", AIOHTTP_OUTPUT),
-    ),
+    "framework",
+    ("flask", "aiohttp"),
 )
-def test_app(testdir, aiohttp_unused_port, framework, expected):
+def test_app(testdir, aiohttp_unused_port, framework):
     # When the testing app is run from CMD
     port = aiohttp_unused_port()
     if platform.system() == "Windows":
@@ -44,7 +36,7 @@ def test_app(testdir, aiohttp_unused_port, framework, expected):
         testdir.run(
             sys.executable, str(HERE / "apps/__init__.py"), str(port), f"--framework={framework}", timeout=timeout
         )
-    with testdir.tmpdir.join("stderr").open() as fd:
-        stderr = fd.read()
+    with testdir.tmpdir.join("stdout").open() as fd:
+        stdout = fd.read()
     # Then it should start OK and emit debug logs
-    assert expected in stderr
+    assert "Schemathesis test server is running!" in stdout
