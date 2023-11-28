54e9ea91d719e40bf001bdb62b846a8d7034ad93
Dmitry Dygalo <dmitry@schemathesis.io>
2023-11-28 13:02:25 +0100

    test: Make tests less flaky

--- test/cli/test_options.py
+++ test/cli/test_options.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 import click
 import pytest
-from hypothesis import assume, example, given
+from hypothesis import assume, example, given, settings
 from hypothesis import strategies as st
 
 from schemathesis.cli import CsvChoice, CsvEnumChoice
@@ -15,6 +15,7 @@ class Options(Enum):
 
 @given(value=st.text() | st.lists(st.text()).map(",".join))
 @example("")
+@settings(deadline=None)
 def test_csv_enum_choice(value):
     option = CsvEnumChoice(Options)
     with pytest.raises(click.BadParameter):
@@ -22,6 +23,7 @@ def test_csv_enum_choice(value):
 
 
 @given(options=st.lists(st.text()), value=st.text() | st.lists(st.text()).map(",".join))
+@settings(deadline=None)
 def test_csv_choice(options, value):
     assume(all(v not in options for v in value.split(",")))
     option = CsvChoice(options)
