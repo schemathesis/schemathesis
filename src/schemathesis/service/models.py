4622165e51cb606196642d5bd90f1d4b14eba0c3
Dmitry Dygalo <dmitry@dygalo.dev>
2023-12-03 17:15:27 +0100

    chore: Always load remote schema for any service projects

--- src/schemathesis/service/models.py
+++ src/schemathesis/service/models.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from enum import Enum
+from typing import Any
 
 
 class UploadSource(str, Enum):
@@ -9,9 +10,26 @@ class UploadSource(str, Enum):
 
 
 @dataclass
-class ApiDetails:
-    location: str
-    base_url: str | None
+class ProjectDetails:
+    environments: list[ProjectEnvironment]
+    specification: Specification
+
+    @property
+    def default_environment(self) -> ProjectEnvironment | None:
+        return next((env for env in self.environments if env.is_default), None)
+
+
+@dataclass
+class ProjectEnvironment:
+    url: str
+    name: str
+    description: str
+    is_default: bool
+
+
+@dataclass
+class Specification:
+    schema: dict[str, Any]
 
 
 @dataclass
