93b640409b4e94eaac1c4aab78fd6c5575fe312c
Dmitry Dygalo <dmitry@dygalo.dev>
2023-12-20 23:20:35 +0100

    feat: An option to generate random examples for API operations that lack explicit examples
    Ref: #1728

--- src/schemathesis/contrib/openapi/__init__.py
+++ src/schemathesis/contrib/openapi/__init__.py
@@ -1,9 +1,11 @@
-from . import formats
+from . import formats, fill_missing_examples
 
 
 def install() -> None:
     formats.install()
+    fill_missing_examples.install()
 
 
 def uninstall() -> None:
     formats.uninstall()
+    fill_missing_examples.uninstall()
---
+++ src/schemathesis/contrib/openapi/fill_missing_examples.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from ...hooks import HookContext, register, unregister
+
+if TYPE_CHECKING:
+    from ...models import Case
+
+
+def install() -> None:
+    register(before_add_examples)
+
+
+def uninstall() -> None:
+    unregister(before_add_examples)
+
+
+def before_add_examples(context: HookContext, examples: list[Case]) -> None:
+    if not examples and context.operation is not None:
+        from ..._hypothesis import add_single_example
+
+        strategy = context.operation.as_strategy()
+        add_single_example(strategy, examples)
