7eb8abc88b47f17af9006b5c0e8db1776a823bd6
Dmitry Dygalo <dmitry@dygalo.dev>
2024-03-06 19:18:49 +0100

    chore: update
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- src/schemathesis/service/extensions.py
+++ src/schemathesis/service/extensions.py
@@ -1,15 +1,24 @@
 from __future__ import annotations
 
-import re
-from typing import Optional, TYPE_CHECKING
+import base64
+from ipaddress import IPv4Network, IPv6Network
+from typing import TYPE_CHECKING, Callable, Optional
 
-from ..exceptions import format_exception
 from ..internal.result import Err, Ok, Result
-from .models import Extension, StringFormatsExtension, SchemaOptimizationExtension, Success, Error
-
+from .models import (
+    Extension,
+    SchemaOptimizationExtension,
+    StrategyDefinition,
+    StringFormatsExtension,
+    Success,
+    TransformFunctionDefinition,
+)
 
 if TYPE_CHECKING:
+    from datetime import date, datetime
+
     from hypothesis import strategies as st
+
     from ..schemas import BaseSchema
 
 
@@ -24,41 +33,12 @@ def apply(extensions: list[Extension], schema: BaseSchema) -> None:
 
 def _apply_string_formats_extension(extension: StringFormatsExtension) -> None:
     from ..specs.openapi import formats
-    from hypothesis import strategies as st
 
     for name, value in extension.formats.items():
-        if "builtin" in value:
-            key = value["builtin"]
-            strat = _find_built_in_strategy(key)
-            if strat is None:
-                extension.set_state(Error(message=f"Unknown builtin strategy: `{key}`"))
-                continue
-            strategy = strat().map(str)
-        elif "regex" in value:
-            regex = value["regex"]
-            try:
-                re.compile(regex)
-            except re.error as exc:
-                extension.set_state(Error(message=f"Invalid regex: `{regex}`", exception=format_exception(exc)))
-                continue
-            strategy = st.from_regex(regex)
-            if "samples" in value:
-                samples = value["samples"]
-                validated = _validate_sampled_from(samples)
-                if isinstance(validated, Err):
-                    extension.set_state(Error(message=str(validated.err())))
-                    continue
-                strategy |= st.sampled_from(samples)
-        elif "samples" in value:
-            samples = value["samples"]
-            validated = _validate_sampled_from(samples)
-            if isinstance(validated, Err):
-                extension.set_state(Error(message=str(validated.err())))
-                continue
-            strategy = st.sampled_from(samples)
-        else:
-            extension.set_state(Error(message="Unsupported string format extension"))
-            continue
+        strategy = strategy_from_definitions(value)
+        # if True:
+        #    extension.set_state(Error(message="Unsupported string format extension"))
+        #    continue
         formats.register(name, strategy)
         extension.set_state(Success())
 
@@ -71,7 +51,8 @@ def _validate_sampled_from(elements: list[str]) -> Result[None, ValueError]:
 
 def _find_built_in_strategy(name: str) -> Optional[st.SearchStrategy]:
     """Find a built-in Hypothesis strategy by its name."""
-    from hypothesis import strategies as st, provisional as pr
+    from hypothesis import provisional as pr
+    from hypothesis import strategies as st
 
     for module in (st, pr):
         if hasattr(module, name):
@@ -83,3 +64,62 @@ def _apply_schema_optimization_extension(extension: SchemaOptimizationExtension,
     """Update the schema with its optimized version."""
     schema.raw_schema = extension.schema
     extension.set_state(Success())
+
+
+def strategy_from_definitions(definitions: list[StrategyDefinition]) -> st.SearchStrategy:
+    from ..utils import combine_strategies
+
+    strategies = []
+    for definition in definitions:
+        strategies.append(_strategy_from_definition(definition))
+    return combine_strategies(strategies)
+
+
+KNOWN_ARGUMENTS = {
+    "IPv4Network": IPv4Network,
+    "IPv6Network": IPv6Network,
+}
+
+
+def _strategy_from_definition(definition: StrategyDefinition) -> st.SearchStrategy:
+    base = _find_built_in_strategy(definition.name)
+    if base is None:
+        raise ValueError(f"Unknown builtin strategy: {definition.name}")
+    arguments = definition.arguments or {}
+    arguments = arguments.copy()
+    for key, value in arguments.items():
+        known = KNOWN_ARGUMENTS.get(value)
+        if known is not None:
+            arguments[key] = known
+    strategy = base(**arguments)
+    for transform in definition.transforms or []:
+        function = _get_transform_function(transform)
+        if transform["kind"] == "map":
+            strategy = strategy.map(function)
+        elif transform["kind"] == "filter":
+            strategy = strategy.filter(function)
+
+    return strategy
+
+
+def make_strftime(format: str) -> Callable:
+    def strftime(value: date | datetime) -> str:
+        return value.strftime(format)
+
+    return strftime
+
+
+def _get_transform_function(definition: TransformFunctionDefinition) -> Callable | None:
+    from ..specs.openapi._hypothesis import Binary
+
+    TRANSFORM_FACTORIES: dict[str, Callable] = {
+        "str": lambda: str,
+        "base64_encode": lambda: lambda x: Binary(base64.b64encode(x)),
+        "urlsafe_base64_encode": lambda: lambda x: Binary(base64.urlsafe_b64encode(x)),
+        "strftime": make_strftime,
+    }
+    factory = TRANSFORM_FACTORIES.get(definition["name"])
+    if factory is None:
+        raise ValueError(f"Unknown transform: {definition['name']}")
+    arguments = definition.get("arguments", {})
+    return factory(**arguments)
