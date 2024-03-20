1582e65f0fc038a4e78d51cd9209a28a5b821fd1
Dmitry Dygalo <dmitry@dygalo.dev>
2024-03-20 17:51:21 +0100

    chore: update
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- src/schemathesis/exceptions.py
+++ src/schemathesis/exceptions.py
@@ -543,11 +543,11 @@ def extract_requests_exception_details(exc: RequestException) -> tuple[str, list
     elif isinstance(exc, ConnectionError):
         message = "Connection failed"
         inner = exc.args[0]
-        if isinstance(inner, MaxRetryError) and inner.reason is not None and isinstance(inner.reason.args[0], str):
-            if ":" not in inner.reason.args[0]:
-                reason = inner.reason.args[0]
+        if isinstance(inner, MaxRetryError) and inner.reason is not None:
+            if ":" not in str(inner.reason.args[0]):
+                reason = str(inner.reason.args[0])
             else:
-                _, reason = inner.reason.args[0].split(":", maxsplit=1)
+                _, reason = str(inner.reason.args[0]).split(":", maxsplit=1)
             extra = [reason.strip()]
         else:
             extra = [" ".join(map(str, inner.args))]
