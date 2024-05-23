a608d39866b5b0066df6b6e288e6f97a5f637ead
Dmitry Dygalo <dmitry@dygalo.dev>
2024-05-23 21:07:54 +0200

    chore: wip
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- benches/schema.py
+++ benches/schema.py
@@ -7,6 +7,7 @@ from hypothesis import HealthCheck, Phase, Verbosity
 
 import schemathesis
 from schemathesis.runner import from_schema
+from schemathesis.specs.openapi._v2 import iter_operations
 
 CURRENT_DIR = pathlib.Path(__file__).parent.absolute()
 CATALOG_DIR = CURRENT_DIR / "data"
@@ -19,175 +20,189 @@ def read_json_from_catalog(path: str):
 
 # Small size (~2k lines in YAML)
 BBCI = read_json_from_catalog("bbci.json")
-BBCI_SCHEMA = schemathesis.from_dict(BBCI)
-BBCI_OPERATIONS = list(BBCI_SCHEMA.get_all_operations())
+# BBCI_SCHEMA = schemathesis.from_dict(BBCI)
+# BBCI_OPERATIONS = list(BBCI_SCHEMA.get_all_operations())
 # Medium size (~8k lines in YAML)
 VMWARE = read_json_from_catalog("vmware.json")
-VMWARE_SCHEMA = schemathesis.from_dict(VMWARE)
-VMWARE_OPERATIONS = list(VMWARE_SCHEMA.get_all_operations())
+# VMWARE_SCHEMA = schemathesis.from_dict(VMWARE)
+# VMWARE_OPERATIONS = list(VMWARE_SCHEMA.get_all_operations())
 # Large size (~92k lines in YAML)
 STRIPE = read_json_from_catalog("stripe.json")
-STRIPE_SCHEMA = schemathesis.from_dict(STRIPE)
+# STRIPE_SCHEMA = schemathesis.from_dict(STRIPE)
 # Medium GraphQL schema (~6k lines)
-UNIVERSE = read_json_from_catalog("universe.json")
-UNIVERSE_SCHEMA = schemathesis.graphql.from_dict(UNIVERSE)
+# UNIVERSE = read_json_from_catalog("universe.json")
+# UNIVERSE_SCHEMA = schemathesis.graphql.from_dict(UNIVERSE)
+
+APPVEYOR = read_json_from_catalog("appveyor.json")
+EVETECH = read_json_from_catalog("evetech.json")
+OSISOFT = read_json_from_catalog("osisoft.json")
 
 
 @pytest.mark.benchmark
 @pytest.mark.parametrize(
-    "raw_schema, loader",
-    [
-        (BBCI, schemathesis.from_dict),
-        (VMWARE, schemathesis.from_dict),
-        (UNIVERSE, schemathesis.graphql.from_dict),
-    ],
-    ids=("bbci", "vmware", "universe"),
+    "raw_schema",
+    [APPVEYOR, EVETECH, OSISOFT],
+    ids=("appveyor", "evetech", "osisoft"),
 )
-def test_get_all_operations(raw_schema, loader):
-    schema = loader(raw_schema)
-
-    for _ in schema.get_all_operations():
-        pass
+def test_iter_operations_v2(raw_schema):
+    _ = list(iter_operations(raw_schema, ""))
 
 
-@pytest.mark.benchmark
-@pytest.mark.parametrize(
-    "raw_schema, loader",
-    [
-        (BBCI, schemathesis.from_dict),
-        (VMWARE, schemathesis.from_dict),
-        (STRIPE, schemathesis.from_dict),
-        (UNIVERSE, schemathesis.graphql.from_dict),
-    ],
-    ids=("bbci", "vmware", "stripe", "universe"),
-)
-def test_length(raw_schema, loader):
-    schema = loader(raw_schema)
-    _ = len(schema)
-
-
-# Schemas with pre-populated cache
-BBCI_OPERATION_ID = "Get_Categories_"
-BBCI_OPERATION_KEY = ("/categories", "get")
-BBCI_SCHEMA_WITH_OPERATIONS_CACHE = schemathesis.from_dict(BBCI)
-BBCI_SCHEMA_WITH_OPERATIONS_CACHE.get_operation_by_id(BBCI_OPERATION_ID)
-VMWARE_OPERATION_ID = "listProblemEvents"
-VMWARE_OPERATION_KEY = ("/entities/problems", "get")
-VMWARE_SCHEMA_WITH_OPERATIONS_CACHE = schemathesis.from_dict(VMWARE)
-VMWARE_SCHEMA_WITH_OPERATIONS_CACHE.get_operation_by_id(VMWARE_OPERATION_ID)
-UNIVERSE_OPERATION_KEY = ("Query", "manageTickets")
-UNIVERSE_SCHEMA_WITH_OPERATIONS_CACHE = schemathesis.graphql.from_dict(UNIVERSE)
-UNIVERSE_SCHEMA_WITH_OPERATIONS_CACHE[UNIVERSE_OPERATION_KEY[0]][UNIVERSE_OPERATION_KEY[1]]
-
-
-@pytest.mark.benchmark
-@pytest.mark.parametrize(
-    "raw_schema, key, loader",
-    [
-        (BBCI, BBCI_OPERATION_KEY, schemathesis.from_dict),
-        (VMWARE, VMWARE_OPERATION_KEY, schemathesis.from_dict),
-        (UNIVERSE, UNIVERSE_OPERATION_KEY, schemathesis.graphql.from_dict),
-    ],
-    ids=("bbci", "vmware", "universe"),
-)
-def test_get_operation_single(raw_schema, key, loader):
-    schema = loader(raw_schema)
-    current = schema
-    for segment in key:
-        current = current[segment]
-
-
-@pytest.mark.benchmark
-@pytest.mark.parametrize(
-    "schema, key",
-    [
-        (BBCI_SCHEMA_WITH_OPERATIONS_CACHE, BBCI_OPERATION_KEY),
-        (VMWARE_SCHEMA_WITH_OPERATIONS_CACHE, VMWARE_OPERATION_KEY),
-        (UNIVERSE_SCHEMA_WITH_OPERATIONS_CACHE, UNIVERSE_OPERATION_KEY),
-    ],
-    ids=("bbci", "vmware", "universe"),
-)
-def test_get_operation_repeatedly(schema, key):
-    current = schema
-    for segment in key:
-        current = current[segment]
-
-
-@pytest.mark.benchmark
-@pytest.mark.parametrize(
-    "raw_schema, key",
-    [(BBCI, BBCI_OPERATION_ID), (VMWARE, VMWARE_OPERATION_ID)],
-    ids=("bbci", "vmware"),
-)
-def test_get_operation_by_id_single(raw_schema, key):
-    schema = schemathesis.from_dict(raw_schema)
-    _ = schema.get_operation_by_id(key)
-
-
-@pytest.mark.benchmark
-@pytest.mark.parametrize(
-    "schema, key",
-    [
-        (BBCI_SCHEMA_WITH_OPERATIONS_CACHE, BBCI_OPERATION_ID),
-        (VMWARE_SCHEMA_WITH_OPERATIONS_CACHE, VMWARE_OPERATION_ID),
-    ],
-    ids=("bbci", "vmware"),
-)
-def test_get_operation_by_id_repeatedly(schema, key):
-    _ = schema.get_operation_by_id(key)
-
-
-@pytest.mark.benchmark
-@pytest.mark.parametrize(
-    "raw_schema, key",
-    [
-        (BBCI, "#/paths/~1categories/get"),
-        (VMWARE, "#/paths/~1entities~1problems/get"),
-    ],
-    ids=("bbci", "vmware"),
-)
-def test_get_operation_by_reference_single(raw_schema, key):
-    schema = schemathesis.from_dict(raw_schema)
-    _ = schema.get_operation_by_reference(key)
-
-
-@pytest.mark.benchmark
-@pytest.mark.parametrize(
-    "schema, key",
-    [
-        (BBCI_SCHEMA_WITH_OPERATIONS_CACHE, "#/paths/~1categories/get"),
-        (VMWARE_SCHEMA_WITH_OPERATIONS_CACHE, "#/paths/~1entities~1problems/get"),
-    ],
-    ids=("bbci", "vmware"),
-)
-def test_get_operation_by_reference_repeatedly(schema, key):
-    _ = schema.get_operation_by_reference(key)
-
-
-@pytest.mark.benchmark
-def test_events():
-    runner = from_schema(
-        BBCI_SCHEMA,
-        checks=(),
-        count_operations=False,
-        count_links=False,
-        hypothesis_settings=hypothesis.settings(
-            deadline=None,
-            database=None,
-            max_examples=1,
-            derandomize=True,
-            suppress_health_check=list(HealthCheck),
-            phases=[Phase.explicit, Phase.generate],
-            verbosity=Verbosity.quiet,
-        ),
-    )
-    for _ in runner.execute():
-        pass
-
-
-@pytest.mark.benchmark
-@pytest.mark.parametrize("raw_schema", [BBCI, VMWARE, STRIPE], ids=("bbci", "vmware", "stripe"))
-def test_links_count(raw_schema):
-    schema = schemathesis.from_dict(raw_schema)
-
-    _ = schema.links_count
+# @pytest.mark.benchmark
+# @pytest.mark.parametrize(
+#     "raw_schema, loader",
+#     [
+#         (BBCI, schemathesis.from_dict),
+#         (VMWARE, schemathesis.from_dict),
+#         (UNIVERSE, schemathesis.graphql.from_dict),
+#     ],
+#     ids=("bbci", "vmware", "universe"),
+# )
+# def test_get_all_operations(raw_schema, loader):
+#     schema = loader(raw_schema)
+#
+#     for _ in schema.get_all_operations():
+#         pass
+#
+#
+# @pytest.mark.benchmark
+# @pytest.mark.parametrize(
+#     "raw_schema, loader",
+#     [
+#         (BBCI, schemathesis.from_dict),
+#         (VMWARE, schemathesis.from_dict),
+#         (STRIPE, schemathesis.from_dict),
+#         (UNIVERSE, schemathesis.graphql.from_dict),
+#     ],
+#     ids=("bbci", "vmware", "stripe", "universe"),
+# )
+# def test_length(raw_schema, loader):
+#     schema = loader(raw_schema)
+#     _ = len(schema)
+#
+#
+# # Schemas with pre-populated cache
+# BBCI_OPERATION_ID = "Get_Categories_"
+# BBCI_OPERATION_KEY = ("/categories", "get")
+# BBCI_SCHEMA_WITH_OPERATIONS_CACHE = schemathesis.from_dict(BBCI)
+# BBCI_SCHEMA_WITH_OPERATIONS_CACHE.get_operation_by_id(BBCI_OPERATION_ID)
+# VMWARE_OPERATION_ID = "listProblemEvents"
+# VMWARE_OPERATION_KEY = ("/entities/problems", "get")
+# VMWARE_SCHEMA_WITH_OPERATIONS_CACHE = schemathesis.from_dict(VMWARE)
+# VMWARE_SCHEMA_WITH_OPERATIONS_CACHE.get_operation_by_id(VMWARE_OPERATION_ID)
+# UNIVERSE_OPERATION_KEY = ("Query", "manageTickets")
+# UNIVERSE_SCHEMA_WITH_OPERATIONS_CACHE = schemathesis.graphql.from_dict(UNIVERSE)
+# UNIVERSE_SCHEMA_WITH_OPERATIONS_CACHE[UNIVERSE_OPERATION_KEY[0]][UNIVERSE_OPERATION_KEY[1]]
+#
+#
+# @pytest.mark.benchmark
+# @pytest.mark.parametrize(
+#     "raw_schema, key, loader",
+#     [
+#         (BBCI, BBCI_OPERATION_KEY, schemathesis.from_dict),
+#         (VMWARE, VMWARE_OPERATION_KEY, schemathesis.from_dict),
+#         (UNIVERSE, UNIVERSE_OPERATION_KEY, schemathesis.graphql.from_dict),
+#     ],
+#     ids=("bbci", "vmware", "universe"),
+# )
+# def test_get_operation_single(raw_schema, key, loader):
+#     schema = loader(raw_schema)
+#     current = schema
+#     for segment in key:
+#         current = current[segment]
+#
+#
+# @pytest.mark.benchmark
+# @pytest.mark.parametrize(
+#     "schema, key",
+#     [
+#         (BBCI_SCHEMA_WITH_OPERATIONS_CACHE, BBCI_OPERATION_KEY),
+#         (VMWARE_SCHEMA_WITH_OPERATIONS_CACHE, VMWARE_OPERATION_KEY),
+#         (UNIVERSE_SCHEMA_WITH_OPERATIONS_CACHE, UNIVERSE_OPERATION_KEY),
+#     ],
+#     ids=("bbci", "vmware", "universe"),
+# )
+# def test_get_operation_repeatedly(schema, key):
+#     current = schema
+#     for segment in key:
+#         current = current[segment]
+#
+#
+# @pytest.mark.benchmark
+# @pytest.mark.parametrize(
+#     "raw_schema, key",
+#     [(BBCI, BBCI_OPERATION_ID), (VMWARE, VMWARE_OPERATION_ID)],
+#     ids=("bbci", "vmware"),
+# )
+# def test_get_operation_by_id_single(raw_schema, key):
+#     schema = schemathesis.from_dict(raw_schema)
+#     _ = schema.get_operation_by_id(key)
+#
+#
+# @pytest.mark.benchmark
+# @pytest.mark.parametrize(
+#     "schema, key",
+#     [
+#         (BBCI_SCHEMA_WITH_OPERATIONS_CACHE, BBCI_OPERATION_ID),
+#         (VMWARE_SCHEMA_WITH_OPERATIONS_CACHE, VMWARE_OPERATION_ID),
+#     ],
+#     ids=("bbci", "vmware"),
+# )
+# def test_get_operation_by_id_repeatedly(schema, key):
+#     _ = schema.get_operation_by_id(key)
+#
+#
+# @pytest.mark.benchmark
+# @pytest.mark.parametrize(
+#     "raw_schema, key",
+#     [
+#         (BBCI, "#/paths/~1categories/get"),
+#         (VMWARE, "#/paths/~1entities~1problems/get"),
+#     ],
+#     ids=("bbci", "vmware"),
+# )
+# def test_get_operation_by_reference_single(raw_schema, key):
+#     schema = schemathesis.from_dict(raw_schema)
+#     _ = schema.get_operation_by_reference(key)
+#
+#
+# @pytest.mark.benchmark
+# @pytest.mark.parametrize(
+#     "schema, key",
+#     [
+#         (BBCI_SCHEMA_WITH_OPERATIONS_CACHE, "#/paths/~1categories/get"),
+#         (VMWARE_SCHEMA_WITH_OPERATIONS_CACHE, "#/paths/~1entities~1problems/get"),
+#     ],
+#     ids=("bbci", "vmware"),
+# )
+# def test_get_operation_by_reference_repeatedly(schema, key):
+#     _ = schema.get_operation_by_reference(key)
+#
+#
+# @pytest.mark.benchmark
+# def test_events():
+#     runner = from_schema(
+#         BBCI_SCHEMA,
+#         checks=(),
+#         count_operations=False,
+#         count_links=False,
+#         hypothesis_settings=hypothesis.settings(
+#             deadline=None,
+#             database=None,
+#             max_examples=1,
+#             derandomize=True,
+#             suppress_health_check=list(HealthCheck),
+#             phases=[Phase.explicit, Phase.generate],
+#             verbosity=Verbosity.quiet,
+#         ),
+#     )
+#     for _ in runner.execute():
+#         pass
+#
+#
+# @pytest.mark.benchmark
+# @pytest.mark.parametrize("raw_schema", [BBCI, VMWARE, STRIPE], ids=("bbci", "vmware", "stripe"))
+# def test_links_count(raw_schema):
+#     schema = schemathesis.from_dict(raw_schema)
+#
+#     _ = schema.links_count
