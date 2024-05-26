86c133516c4cfcb93b6c674173290468fb08676e
Dmitry Dygalo <dmitry@dygalo.dev>
2024-05-26 22:07:09 +0200

    chore: more
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- src/schemathesis/internal/copy.py
+++ src/schemathesis/internal/copy.py
@@ -2,14 +2,30 @@ from typing import Any, MutableMapping, Mapping
 
 
 def fast_deepcopy(value: Any) -> Any:
-    """A specialized version of `deepcopy` that copies only `dict` and `list`.
+    """A specialized version of `deepcopy` that copies only `dict` and `list` and does unrolling.
 
     It is on average 3x faster than `deepcopy` and given the amount of calls, it is an important optimization.
     """
     if isinstance(value, dict):
-        return {key: fast_deepcopy(v) for key, v in value.items()}
+        return {
+            k1: (
+                {k2: fast_deepcopy(v2) for k2, v2 in v1.items()}
+                if isinstance(v1, dict)
+                else [fast_deepcopy(v2) for v2 in v1]
+                if isinstance(v1, list)
+                else v1
+            )
+            for k1, v1 in value.items()
+        }
     if isinstance(value, list):
-        return [fast_deepcopy(v) for v in value]
+        return [
+            {k2: fast_deepcopy(v2) for k2, v2 in v1.items()}
+            if isinstance(v1, dict)
+            else [fast_deepcopy(v2) for v2 in v1]
+            if isinstance(v1, list)
+            else v1
+            for v1 in value
+        ]
     return value
 
 
