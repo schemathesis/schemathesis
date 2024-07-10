d648efa6543d70e3673e0ab3bf307d082c8dead0
Dmitry Dygalo <dmitry@dygalo.dev>
2024-07-10 22:25:12 +0200

    test: Use hypothesis-openapi for testing with random Open API schemas
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- test/specs/openapi/test_randomized.py
+++ test/specs/openapi/test_randomized.py
@@ -2,7 +2,6 @@ import sys
 
 import pytest
 from hypothesis import Phase, given, settings
-from hypothesis import strategies as st
 
 import schemathesis
 import schemathesis.runner
@@ -11,14 +10,14 @@ from schemathesis.runner import events
 if sys.version_info < (3, 10):
     pytest.skip("Required Python 3.10+", allow_module_level=True)
 
-from .factory import asdict
-from .v2 import Swagger
+from hypothesis_openapi import openapis
 
 
-@given(st.from_type(Swagger).map(asdict))
+@given(openapis(version="2.0") | openapis(version="3.0"))
 @settings(max_examples=25, phases=[Phase.generate], deadline=None)
-def test_swagger(schema):
+def test_random_schemas(schema):
     schema = schemathesis.from_dict(schema, validate_schema=True)
+    # Disable schema validation to allow more flexible behavior at runtime
     schema.validate_schema = False
     for event in schemathesis.runner.from_schema(schema, dry_run=True).execute():
         assert not isinstance(event, events.InternalError), repr(event)
