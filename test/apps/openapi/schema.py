9c56f77b68d218529e01bc519b05e62223a8a3dc
Dmitry Dygalo <dmitry@dygalo.dev>
2025-02-10 00:46:57 +0100

    feat: Warning for operations that return only 4xx responses during unit tests
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- test/apps/openapi/schema.py
+++ test/apps/openapi/schema.py
@@ -29,6 +29,7 @@ class Operation(Enum):
     teapot = ("POST", "/api/teapot")
     text = ("GET", "/api/text")
     cp866 = ("GET", "/api/cp866")
+    always_incorrect = ("GET", "/api/always_incorrect")
     conformance = ("GET", "/api/conformance")
     plain_text_body = ("POST", "/api/text")
     csv_payload = ("POST", "/api/csv")
