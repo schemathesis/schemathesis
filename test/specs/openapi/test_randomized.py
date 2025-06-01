450d40ce7364415b8549c149ccdfe685a33e7c64
Dmitry Dygalo <dmitry@dygalo.dev>
2025-06-01 12:13:46 +0200

    test: mock network in `test_random_schemas`
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- test/specs/openapi/test_randomized.py
+++ test/specs/openapi/test_randomized.py
@@ -7,6 +7,7 @@ from hypothesis import HealthCheck, Phase, given, settings
 import schemathesis
 import schemathesis.engine
 from schemathesis.config import HealthCheck as SchemathesisHealthCheck
+from schemathesis.config import SchemathesisConfig
 from schemathesis.core.errors import InvalidSchema
 from schemathesis.engine import events
 
@@ -16,15 +17,17 @@ if sys.version_info < (3, 10):
 from hypothesis_openapi import openapis
 
 IGNORED_EXCEPTIONS = (hypothesis.errors.Unsatisfiable, InvalidSchema, hypothesis.errors.FailedHealthCheck)
+config = SchemathesisConfig.from_dict({})
+config.projects.default.update(suppress_health_check=[SchemathesisHealthCheck.all])
+config.projects.default.phases.update(phases=["examples", "fuzzing"])
+config.projects.default.generation.update(max_examples=10)
 
 
 @given(schema=openapis(version="2.0") | openapis(version="3.0"))
 @settings(max_examples=20, phases=[Phase.generate], deadline=None, suppress_health_check=list(HealthCheck))
-def test_random_schemas(schema, openapi3_base_url):
-    schema = schemathesis.openapi.from_dict(schema)
-    schema.config.update(base_url=openapi3_base_url, suppress_health_check=[SchemathesisHealthCheck.all])
-    schema.config.phases.update(phases=["examples", "fuzzing"])
-    schema.config.generation.update(max_examples=10)
+@pytest.mark.usefixtures("mocked_call")
+def test_random_schemas(schema):
+    schema = schemathesis.openapi.from_dict(schema, config=config)
     for event in schemathesis.engine.from_schema(schema).execute():
         assert not isinstance(event, events.FatalError), repr(event)
         if isinstance(event, events.NonFatalError) and not isinstance(event.value, IGNORED_EXCEPTIONS):
