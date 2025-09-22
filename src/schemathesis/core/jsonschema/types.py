c2b4531aa71d1e5aa9ce647334d1389b05e0dd44
Dmitry Dygalo <dmitry@dygalo.dev>
2025-09-22 09:25:24 +0200

    fix: reference collection
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- src/schemathesis/core/jsonschema/types.py
+++ src/schemathesis/core/jsonschema/types.py
@@ -1,8 +1,15 @@
-from typing import Any
+from typing import Any, Union
+
+JsonSchemaObject = dict[str, Any]
+JsonSchema = Union[JsonSchemaObject, bool]
+
+ANY_TYPE = ["null", "boolean", "integer", "number", "string", "array", "object"]
 
 
-def get_type(schema: dict[str, Any]) -> list[str]:
-    type_ = schema.get("type", ["null", "boolean", "integer", "number", "string", "array", "object"])
+def get_type(schema: JsonSchema) -> list[str]:
+    if isinstance(schema, bool):
+        return ANY_TYPE
+    type_ = schema.get("type", ANY_TYPE)
     if isinstance(type_, str):
         return [type_]
     return type_
