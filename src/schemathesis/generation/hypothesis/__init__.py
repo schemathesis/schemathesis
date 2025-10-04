6ce57037f1e8dc940bc1cd53567b5b73a8389e43
Dmitry Dygalo <dmitry@dygalo.dev>
2025-10-04 19:21:42 +0200

    perf: Add more patches for hypothesis-jsonschema
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- src/schemathesis/generation/hypothesis/__init__.py
+++ src/schemathesis/generation/hypothesis/__init__.py
@@ -1,4 +1,7 @@
-from typing import Any
+from __future__ import annotations
+
+from functools import lru_cache
+from typing import Any, Literal
 
 
 def setup() -> None:
@@ -9,8 +12,10 @@ def setup() -> None:
     from hypothesis.strategies._internal import collections, core
     from hypothesis.vendor import pretty
     from hypothesis_jsonschema import _canonicalise, _from_schema, _resolve
+    from hypothesis_jsonschema._canonicalise import SCHEMA_KEYS, SCHEMA_OBJECT_KEYS, merged
+    from hypothesis_jsonschema._resolve import LocalResolver
 
-    from schemathesis.core import INTERNAL_BUFFER_SIZE
+    from schemathesis.core import INTERNAL_BUFFER_SIZE, json
     from schemathesis.core.jsonschema.types import _get_type
     from schemathesis.core.transforms import deepclone
 
@@ -35,11 +40,80 @@ def setup() -> None:
             # depending on the schema size (~300 seconds -> 4.5 seconds in one of the benchmarks)
             return None
 
+    class CacheableSchema:
+        """Cache schema by its JSON representation.
+
+        Canonicalisation is not required as schemas with the same JSON representation
+        will have the same validator.
+        """
+
+        __slots__ = ("schema", "encoded")
+
+        def __init__(self, schema: dict[str, Any]) -> None:
+            self.schema = schema
+            self.encoded = hash(json.dumps(schema, sort_keys=True))
+
+        def __eq__(self, other: "CacheableSchema") -> bool:  # type: ignore
+            return self.encoded == other.encoded
+
+        def __hash__(self) -> int:
+            return self.encoded
+
+    SCHEMA_KEYS = frozenset(SCHEMA_KEYS)
+    SCHEMA_OBJECT_KEYS = frozenset(SCHEMA_OBJECT_KEYS)
+
+    @lru_cache()
+    def get_resolver(cache_key: CacheableSchema) -> LocalResolver:
+        """LRU resolver cache."""
+        return LocalResolver.from_schema(cache_key.schema)
+
+    def resolve_all_refs(
+        schema: Literal[True, False] | dict[str, Any],
+        *,
+        resolver: LocalResolver | None = None,
+    ) -> dict[str, Any]:
+        if schema is True:
+            return {}
+        if schema is False:
+            return {"not": {}}
+        if not schema:
+            return schema
+        if resolver is None:
+            resolver = get_resolver(CacheableSchema(schema))
+
+        _resolve_all_refs = resolve_all_refs
+
+        if "$ref" in schema:
+            s = dict(schema)
+            ref = s.pop("$ref")
+            url, resolved = resolver.resolve(ref)
+            resolver.push_scope(url)
+            try:
+                return merged([s, _resolve_all_refs(deepclone(resolved), resolver=resolver)])  # type: ignore
+            finally:
+                resolver.pop_scope()
+
+        for key, value in schema.items():
+            if key in SCHEMA_KEYS:
+                if isinstance(value, list):
+                    schema[key] = [_resolve_all_refs(v, resolver=resolver) if isinstance(v, dict) else v for v in value]
+                elif isinstance(value, dict):
+                    schema[key] = _resolve_all_refs(value, resolver=resolver)
+            if key in SCHEMA_OBJECT_KEYS:
+                schema[key] = {
+                    k: _resolve_all_refs(v, resolver=resolver) if isinstance(v, dict) else v for k, v in value.items()
+                }
+        return schema
+
     root_core.RepresentationPrinter = RepresentationPrinter  # type: ignore
     _resolve.deepcopy = deepclone  # type: ignore
+    _resolve.resolve_all_refs = resolve_all_refs  # type: ignore
     _from_schema.deepcopy = deepclone  # type: ignore
     _from_schema.get_type = _get_type  # type: ignore
+    _from_schema.resolve_all_refs = resolve_all_refs  # type: ignore
     _canonicalise.get_type = _get_type  # type: ignore
+    _canonicalise.CacheableSchema = CacheableSchema  # type: ignore
+    _canonicalise.json = json  # type: ignore
     root_core.BUFFER_SIZE = INTERNAL_BUFFER_SIZE  # type: ignore
     engine.BUFFER_SIZE = INTERNAL_BUFFER_SIZE
     collections.BUFFER_SIZE = INTERNAL_BUFFER_SIZE  # type: ignore
