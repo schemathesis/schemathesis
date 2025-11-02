ae135d9d50170b564f958a3cba700493f2dc0e2b
Dmitry Dygalo <dmitry@dygalo.dev>
2025-11-02 16:47:04 +0100

    refactor: Use more typing
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- src/schemathesis/specs/openapi/types/v3.py
+++ src/schemathesis/specs/openapi/types/v3.py
@@ -1,30 +1,22 @@
+"""OpenAPI 3.0 and 3.1 type definitions."""
+
 from __future__ import annotations
 
-from typing import Any, Mapping, TypedDict, Union
+from typing import Any, Literal, Mapping, Union
 
-from typing_extensions import NotRequired
+from typing_extensions import NotRequired, TypeAlias, TypedDict
 
-Reference = TypedDict("Reference", {"$ref": str})
-
-
-class Operation(TypedDict):
-    responses: Responses
-    requestBody: NotRequired[RequestBodyOrRef]
-
-
-class RequestBody(TypedDict):
-    content: dict[str, MediaType]
-    required: NotRequired[bool]
-
-
-class MediaType(TypedDict):
-    schema: Schema
-    example: Any
+from schemathesis.specs.openapi.types.common import Reference, SchemaOrRef, _SecurityTypeKey
 
 
 class Example(TypedDict):
     value: NotRequired[Any]
     externalValue: NotRequired[str]
+
+
+class MediaType(TypedDict):
+    schema: SchemaOrRef
+    example: NotRequired[Any]
 
 
 class Link(TypedDict):
@@ -35,10 +27,19 @@ class Link(TypedDict):
     server: NotRequired[Any]
 
 
+class Header(TypedDict):
+    required: NotRequired[bool]
+
+
 class Response(TypedDict):
     headers: NotRequired[dict[str, HeaderOrRef]]
     content: NotRequired[dict[str, MediaType]]
     links: NotRequired[dict[str, LinkOrRef]]
+
+
+class RequestBody(TypedDict):
+    content: dict[str, MediaType]
+    required: NotRequired[bool]
 
 
 _ResponsesBase = Mapping[str, Union[Response, Reference]]
@@ -48,10 +49,6 @@ class Responses(_ResponsesBase):
     pass
 
 
-class Header(TypedDict):
-    required: NotRequired[bool]
-
-
 _HeadersBase = Mapping[str, Union[Header, Reference]]
 
 
@@ -59,10 +56,79 @@ class Headers(_HeadersBase):
     pass
 
 
-SchemaObject = TypedDict("SchemaObject", {"$ref": str})
-Schema = Union[SchemaObject, bool]
-RequestBodyOrRef = Union[RequestBody, Reference]
-ExampleOrRef = Union[Example, Reference]
-HeaderOrRef = Union[Header, Reference]
-LinkOrRef = Union[Link, Reference]
-ResponseOrRef = Union[Response, Reference]
+ExampleOrRef: TypeAlias = Union[Example, Reference]
+"""Example definition or reference."""
+
+HeaderOrRef: TypeAlias = Union[Header, Reference]
+"""Header definition or reference."""
+
+LinkOrRef: TypeAlias = Union[Link, Reference]
+"""Link definition or reference."""
+
+ResponseOrRef: TypeAlias = Union[Response, Reference]
+"""Response definition or reference."""
+
+RequestBodyOrRef: TypeAlias = Union[RequestBody, Reference]
+"""Request body definition or reference."""
+
+
+class ParameterWithSchema(TypedDict):
+    """OpenAPI 3.0/3.1 parameter with schema."""
+
+    name: str
+    description: NotRequired[str]
+    required: NotRequired[bool]
+    deprecated: NotRequired[bool]
+    allowEmptyValue: NotRequired[bool]
+    schema: SchemaOrRef
+    style: NotRequired[str]
+    explode: NotRequired[bool]
+    allowReserved: NotRequired[bool]
+    example: NotRequired[Any]
+    examples: NotRequired[dict[str, ExampleOrRef]]
+
+
+_ParameterIn = TypedDict("_ParameterIn", {"in": Literal["path", "query", "header", "cookie"]})
+
+
+class ParameterWithSchemaAndIn(ParameterWithSchema, _ParameterIn):
+    """Parameter with schema and 'in' field."""
+
+    pass
+
+
+class ParameterWithContent(TypedDict):
+    """OpenAPI 3.0/3.1 parameter with content."""
+
+    name: str
+    description: NotRequired[str]
+    required: NotRequired[bool]
+    deprecated: NotRequired[bool]
+    content: dict[str, MediaType]
+
+
+class ParameterWithContentAndIn(ParameterWithContent, _ParameterIn):
+    """Parameter with content and 'in' field."""
+
+    pass
+
+
+Parameter: TypeAlias = Union[ParameterWithSchemaAndIn, ParameterWithContentAndIn, Reference]
+"""OpenAPI 3.x parameter (with schema, with content, or reference)."""
+
+
+class Operation(TypedDict):
+    responses: Responses
+    requestBody: NotRequired[RequestBodyOrRef]
+    parameters: NotRequired[list[Parameter]]
+
+
+# Security parameter types
+class SecurityParameter(ParameterWithSchema, _SecurityTypeKey):
+    """OpenAPI 3.x synthetic security parameter.
+
+    Created from security schemes (apiKey or http auth).
+    Follows the same structure as ParameterWithSchema since v3 uses nested schema.
+    """
+
+    pass
