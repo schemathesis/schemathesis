16d5ca24f40421e227ae34a9f8479c76c204ae52
Dmitry Dygalo <dmitry@dygalo.dev>
2025-11-07 18:18:18 +0100

    refactor: Move more logic to adapters
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- src/schemathesis/specs/openapi/adapter/protocol.py
+++ src/schemathesis/specs/openapi/adapter/protocol.py
@@ -21,6 +21,7 @@ if TYPE_CHECKING:
     from schemathesis.core.adapter import OperationParameter
     from schemathesis.core.compat import RefResolver
     from schemathesis.core.jsonschema.types import JsonSchema
+    from schemathesis.core.transport import Response
     from schemathesis.schemas import APIOperation
 
 IterResponseExamples = Callable[[Mapping[str, Any], str], Iterator[tuple[str, object]]]
@@ -36,6 +37,12 @@ PrepareMultipart = Callable[
     ["APIOperation", dict[str, Any]],
     Tuple[Optional[List[Tuple[str, Any]]], Optional[dict[str, Any]]],
 ]
+GetResponseContentTypes = Callable[["APIOperation", "Response"], List[str]]
+GetRequestPayloadContentTypes = Callable[["APIOperation"], List[str]]
+GetDefaultMediaTypes = Callable[[Mapping[str, Any]], List[str]]
+GetBasePath = Callable[[Mapping[str, Any]], str]
+ValidateSchema = Callable[[Mapping[str, Any]], None]
+GetParameterSerializer = Callable[[List[dict[str, Any]]], Optional[Callable]]
 IterParameters = Callable[
     [Mapping[str, Any], Sequence[Mapping[str, Any]], list[str], "RefResolver", "SpecificationAdapter"],
     Iterable["OperationParameter"],
@@ -73,6 +80,12 @@ class SpecificationAdapter(Protocol):
     # Function to extract security parameters for an API operation
     extract_security_parameters: ExtractSecurityParameters
     prepare_multipart: PrepareMultipart
+    get_response_content_types: GetResponseContentTypes
+    get_request_payload_content_types: GetRequestPayloadContentTypes
+    get_default_media_types: GetDefaultMediaTypes
+    get_base_path: GetBasePath
+    validate_schema: ValidateSchema
+    get_parameter_serializer: GetParameterSerializer
 
     # JSON Schema validator class appropriate for this specification version
     jsonschema_validator_cls: type[Validator]
