e0d032373c76a910fa66e56b61860cd50be7d890
Dmitry Dygalo <dmitry@dygalo.dev>
2026-01-30 00:23:09 +0100

    chore: Use `jsonschema-rs` for config validation
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- src/schemathesis/config/_validator.py
+++ src/schemathesis/config/_validator.py
@@ -1,9 +1,9 @@
 import json
 from pathlib import Path
 
-import jsonschema.validators
+import jsonschema_rs
 
 with (Path(__file__).absolute().parent / "schema.json").open() as fd:
     CONFIG_SCHEMA = json.loads(fd.read())
 
-CONFIG_VALIDATOR = jsonschema.validators.Draft202012Validator(CONFIG_SCHEMA)
+CONFIG_VALIDATOR = jsonschema_rs.Draft202012Validator(CONFIG_SCHEMA)
