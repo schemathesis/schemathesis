49007ab6c5de834c3b908cebb2a92f1f8cec342d
Dmitry Dygalo <dmitry@dygalo.dev>
2026-05-09 11:58:39 +0200

    chore: Recognize `text/json` as JSON media type
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- test/test_serialization.py
+++ test/test_serialization.py
@@ -529,6 +529,7 @@ def foo(ctx, value):
         ("application/json", {"application/json"}),
         ("application/problem+json", {"application/problem+json"}),
         ("application/jwt", {"application/jwt"}),
+        ("text/json", {"text/json"}),
         (
             "application/*",
             {
