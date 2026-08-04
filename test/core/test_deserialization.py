5d693c0ef85d27f6b6c8c74aea120027712aa110
Dmitry Dygalo <dmitry@dygalo.dev>
2026-08-04 16:35:44 +0200

    fix: Read unquoted `on`, `off`, `yes` and `no` in YAML documents as strings, per YAML 1.2
    Signed-off-by: Dmitry Dygalo <dmitry@dygalo.dev>

--- test/core/test_deserialization.py
+++ test/core/test_deserialization.py
@@ -9,9 +9,18 @@ from schemathesis.core.deserialization import deserialize_yaml
         ("'1': foo", {"1": "foo"}),
         ("1: foo", {"1": "foo"}),
         ("1: 1", {"1": 1}),
-        ("on: off", {"on": False}),
+        ("on: off", {"on": "off"}),
+        ("yes: no", {"yes": "no"}),
+        ("true: false", {"true": False}),
     ],
-    ids=["string-key-string-value", "int-key-string-value", "int-key-int-value", "bool-key-bool-value"],
+    ids=[
+        "string-key-string-value",
+        "int-key-string-value",
+        "int-key-int-value",
+        "yaml-1.1-bool-key-and-value",
+        "yaml-1.1-bool-word-key-and-value",
+        "bool-key-bool-value",
+    ],
 )
 def test_deserialize_yaml(value, expected):
     assert deserialize_yaml(value) == expected
