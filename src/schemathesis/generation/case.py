39b8aa80d0adea5f57f92e6b1e4814503e46ca8c
vyuroshchin <>
2026-08-21 10:18:28 +0300

    chore: drop httpx, add httpx2

--- src/schemathesis/generation/case.py
+++ src/schemathesis/generation/case.py
@@ -41,7 +41,7 @@ from schemathesis.transport.prepare import prepare_path, prepare_request
 from schemathesis.transport.serialization import Binary
 
 if TYPE_CHECKING:
-    import httpx
+    import httpx2
     import requests
     import requests.auth
     from requests.structures import CaseInsensitiveDict
@@ -454,7 +454,7 @@ class Case(Generic[OperationT]):
 
     def validate_response(
         self,
-        response: Response | httpx.Response | requests.Response | TestResponse,
+        response: Response | httpx2.Response | requests.Response | TestResponse,
         checks: list[CheckFunction] | None = None,
         additional_checks: list[CheckFunction] | None = None,
         excluded_checks: list[CheckFunction] | None = None,
